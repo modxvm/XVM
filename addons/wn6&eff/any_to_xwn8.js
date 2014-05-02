0f9f5e41491f72fd84d7a55e439e9e01a1bae028
seriych <seriych@gmail.com>
2014-05-02 23:24:06 +0400

    Add new features to scripts for change ratings in configs

--- addons/wn6&eff/any_to_xwn8.js
+++ addons/wn6&eff/any_to_xwn8.js
@@ -1,11 +1,27 @@
 // Скрипт для замены одного представления рейтинга на другое в конфиге XVM 5.3.0+
+// script.js [rating] [file1 folder1 folder2 folder3 file3 ...]
 // http://www.koreanrandom.com/forum/topic/3163-/#entry43725
 
+// Введите название рейтинга маленькими буквами кавычках
+var rating = "xwn8"; // Примеры: "eff", "xeff", "wn8", "xwn8", "wn6", "xwn6", "wn", "xwn"
+
+// массив поддерживаемых рейтингов
+var Aratings = [ "eff", "xeff", "wn8", "xwn8", "wn6", "xwn6", "wn", "xwn" ];
+
+// если в качестве первого аргумента передан какой-то рейтинг, применяем его для замен в файлах
+if ( arg0isRating()==1 )
+    rating = WScript.Arguments(0);
+
 // нужный текст
-var text_rate   = "{{xwn8%2s|--}}";
-var text_color  = "{{c:xwn8}}";
-var text_alpha  = "{{a:xwn8}}";
-var text_option = "\"xwnInCompany\": true";
+if ( rating.indexOf("x")==0 )
+    var text_rate   = "{{" + rating + "%2s|--}}"
+else
+    var text_rate   = "{{" + rating + "%4d|----}}";
+var text_color      = "{{c:" + rating + "}}";
+var text_alpha      = "{{a:" + rating + "}}";
+var text_option     = "\"xwnInCompany\": false";
+if ( rating.indexOf("wn")>=0 )
+    text_option     = "\"xwnInCompany\": true";
 
 // заменяемый текст
 var find_rate   = /{{x?(wn|eff)[^}]*}}/g;
@@ -13,27 +29,50 @@ var find_color  = /{{c:x?(wn|eff)[^}]*}}/g;
 var find_alpha  = /{{a:x?(wn|eff)[^}]*}}/g;
 var find_option = /"xwnInCompany"[\s\t]*:[\s\t]*(true|false)/g;
 
-var i=0;
-do {
-    // Имя файла берём из аргумента или задаем XVM.xc, если аргумент пуст
-    var file_name = "xvm.xc";
-    if ( WScript.Arguments.length > 0 )
-        file_name = WScript.Arguments(i);
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var fileList = "";
 
-    var fso=WScript.CreateObject("Scripting.FileSystemObject");
+// Если не переданы файлы или папки в качестве аргументов, меняем во всех *.xc файлах в папке со скриптом
+if ( WScript.Arguments.length == arg0isRating() )
+	replaceInFolder( WScript.ScriptFullName.substr(0, (WScript.ScriptFullName.length - WScript.ScriptName.length)) )
+else  // если переданы аргументы, кроме рейтинга, меняем в них
+    for ( var i=arg0isRating(); i<WScript.Arguments.length; i++)
+        // если аргумент- файл, меняем в нем
+        if ( fso.FileExists(WScript.Arguments(i)) )
+            replaceInFile( WScript.Arguments(i) )
+		// если аргумент- папка, меняем в файлах в ней
+        else if ( fso.FolderExists(WScript.Arguments(i)) )
+			replaceInFolder( WScript.Arguments(i) )
+
+//WScript.Echo(fileList);
+// выходим из программы
+WScript.Quit();
+
+// возвращаем 1, если первый аргумент- какой-то из поддерживаемых рейтингов
+function arg0isRating() {
+    if ( WScript.Arguments.length > 0 )
+        for (var i=0; i<Aratings.length; i++)
+            if ( WScript.Arguments(0)==Aratings[i] )
+                return 1;
+    return 0;
+}
+
+// функция замены рейтингов в файле
+function replaceInFile(file_name) {
+    var fs = WScript.CreateObject("Scripting.FileSystemObject");
     // если не найден файл, переходим к следующему
-    if ( !fso.FileExists(file_name) )
-        break;
+    if ( !fs.FileExists(file_name) )
+        return;
 
     // Переносим исходный файл во временный
     var file_name_tmp = file_name+".tmp";
-    if ( fso.FileExists(file_name_tmp) )
-        fso.DeleteFile(file_name_tmp);
-    fso.MoveFile(file_name, file_name_tmp);
+    if ( fs.FileExists(file_name_tmp) )
+        fs.DeleteFile(file_name_tmp);
+    fs.MoveFile(file_name, file_name_tmp);
 
     // создаем переменные для доступа к файлам
-    var fold = fso.OpenTextFile(file_name_tmp,1,false,false);
-    var fnew = fso.OpenTextFile(file_name,2,true,false);
+    var fold = fs.OpenTextFile(file_name_tmp,1,false,false);
+    var fnew = fs.OpenTextFile(file_name,2,true,false);
 
     // читаем старый конфиг, пока не закончится
     while ( !fold.AtEndOfStream ) {
@@ -52,10 +91,16 @@ do {
     // закрываем файлы и удаляем временный
     fold.Close();
     fnew.Close();
-    fso.DeleteFile(file_name_tmp);  // добавьте // вначале строки, чтобы оставались резервные копии файлов *.old
+    fs.DeleteFile(file_name_tmp);  // добавьте // вначале строки, чтобы оставались резервные копии файлов *.old
+    fileList = fileList + "\n" + file_name;
+}
 
-    i++
-} while (i<WScript.Arguments.length);
-
-// выходим из программы
-WScript.Quit();
+// функция замены рейтингов в папке
+function replaceInFolder(folder_name) {
+	var folderObj = fso.GetFolder(folder_name);
+	var folderC = new Enumerator(folderObj.files);
+	// меняем во всех *.xc, *.XC, *.xvmconf и *.XVMconf файлах внутри папки
+	for (; !folderC.atEnd(); folderC.moveNext())
+		if ( /(\.xc|\.XC|\.xvmconf|\.XVMconf)$/.test(folderC.item()) )
+			replaceInFile( ""+folderC.item() );
+}
