2a43b83937018784ef4a1b8bfa7debf461d68fd9
sirmax2 <devnull@localhost>
2014-12-25 01:40:42 +0200

    XVM-5.4.2

--- utils/test.sh
+++ utils/test.sh
@@ -18,7 +18,7 @@ done
 
 CURRENT_DIRECTORY=`pwd`
 
-#SAMPLE_REPLAY=test.wotreplay
+SAMPLE_REPLAY=test.wotreplay
 #SAMPLE_REPLAY=test1.wotreplay
 #SAMPLE_REPLAY=test2.wotreplay
 #SAMPLE_REPLAY=test3.wotreplay
