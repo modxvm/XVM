c180a97ac9b4a8b66d5794bbbca0f27755e09929
Mikhail Paulyshka <me@mixaill.tk>
2015-01-05 00:51:37 +0300

    [XFW] cleanup

--- src/xvm/build-xfw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export xfw_path_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export xfw_output_swc_path="lib"
-export xfw_output_swf_path="../../bin/mods/xfw/actionscript/"
-
-pushd ../xfw/src/actionscript/ >/dev/null
-./build.sh
-popd >/dev/null
