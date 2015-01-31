e449df078fc6dea0b1f0ddc4988c69a76f846918
sirmax2 <devnull@localhost>
2015-02-01 00:52:32 +0200

    xvm_export

--- release/configs/default/export.xc
+++ release/configs/default/export.xc
@@ -9,6 +9,7 @@
     "fps": {
       "enabled": false,
       "interval": 0.1, // sec
+      "outputDir": "replays"
     }
   }
 }
--- src/xpm/xvm_export/fps.py
+++ src/xpm/xvm_export/fps.py
@@ -10,6 +10,8 @@ def stop():
 
 # PRIVATE
 
+import datetime
+
 import BigWorld
 
 from xfw import *
@@ -35,27 +37,44 @@ class _Fps():
         if config.config['export']['fps']['enabled']:
             #debug('fps start')
             self.interval = config.config['export']['fps']['interval']
+            self.outputDir = config.config['export']['fps']['outputDir']
             self.intervalId = BigWorld.callback(self.interval, self.update)
+            self.values = []
 
     def stop(self):
         #debug('fps stop')
         if self.intervalId is not None:
             BigWorld.cancelCallback(self.intervalId)
             self.intervalId = None
+        if len(self.values):
+            #log(self.values)
+            try:
+                if not os.path.isdir(self.outputDir):
+                    os.makedirs(self.outputDir)
+            except:
+                LOG_ERROR('Failed to create directory for fps files')
+                return
+            fileName = '{0}/fps-{1}.csv'.format(self.outputDir, datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+            with open(fileName, 'w') as f:
+                f.write('Time;Fps\n')
+                for item in self.values:
+                    f.write("{0};{1}\n".format(round(item['time'], 3), round(item['fps'], 3)))
+            self.values = []
 
     def update(self):
         #debug('update')
         period = getArenaPeriod()
-        time = max(0, int(BigWorld.time() * 1000))
+        time = BigWorld.time()
         fps = BigWorld.getFPS()[1]
 
-        if period == 3:
+        if period == 3 and time > 0:
             self.add_value(period, time, fps)
 
         self.intervalId = BigWorld.callback(self.interval, self.update)
 
     def add_value(self, period, time, fps):
         #debug('fps: {0} per: {1} time: {2}'.format(fps, period, time))
+        self.values.append({'period':period,'time':time,'fps':fps})
         pass
 
 _g_fps = None
