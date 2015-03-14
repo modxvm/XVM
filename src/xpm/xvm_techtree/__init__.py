c447dbd4ff57d7e6ad35a56962b336630cf69e3d
Yaroslav Brustinov <monk143@bk.ru>
2015-03-14 16:43:44 +0200

    1) removing not needed config option 2) disable return previous crew if full crew 3) add shooting ranges information in gun info window

--- src/xpm/xvm_techtree/__init__.py
+++ src/xpm/xvm_techtree/__init__.py
@@ -24,11 +24,32 @@ def ItemsData_getAllPossibleXP(base, self, nodeCD, unlockStats):
         return unlockStats.getVehTotalXP(nodeCD)
     return base(self, nodeCD, unlockStats)
 
+# add shooting range in gun info window for SPG/machine guns
+def ModuleInfoWindow_as_setModuleInfoS(base, self, moduleInfo):
+    try:
+        if moduleInfo['type'] == 'vehicleGun':
+            from gui.shared import g_itemsCache
+            veh_id = self._ModuleInfoWindow__vehicleDescr.type.compactDescr
+            vehicle = g_itemsCache.items.getItemByCD(veh_id)
+            gun = g_itemsCache.items.getItemByCD(self.moduleCompactDescr).descriptor
+            turret = self._ModuleInfoWindow__vehicleDescr.turret    # not accurate, but not relevant here
+            (viewRange, shellRadius, artiRadius) = _getRanges(turret, gun, vehicle.nationName, vehicle.type)
+            if vehicle.type == 'SPG':   # arti
+                moduleInfo['parameters']['params'].append({'type': '<h>' + l10n('shootingRadius') + ' <p>' + l10n('(m)') + '</p></h>', 'value': '<h>' + str(artiRadius) + '</h>'})
+            elif shellRadius < 707:     # not arti, short range weapons
+                moduleInfo['parameters']['params'].append({'type': '<h>' + l10n('shootingRadius') + ' <p>' + l10n('(m)') + '</p></h>', 'value': '<h>' + str(shellRadius) + '</h>'})
+    except Exception, ex:
+        err(traceback.format_exc())
+    return base(self, moduleInfo)
+
+
 #####################################################################
 # Register events
 
 def _RegisterEvents():
     from gui.Scaleform.daapi.view.lobby.techtree.data import _ItemsData
     OverrideMethod(_ItemsData, '_getAllPossibleXP', ItemsData_getAllPossibleXP)
+    from gui.Scaleform.daapi.view.meta.ModuleInfoMeta import ModuleInfoMeta
+    OverrideMethod(ModuleInfoMeta, 'as_setModuleInfoS', ModuleInfoWindow_as_setModuleInfoS)
 
 BigWorld.callback(0, _RegisterEvents)
