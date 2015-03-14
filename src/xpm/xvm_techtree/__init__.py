ebd3203ca47c7d0485d8db47a5cd6908985a05c3
sirmax2 <devnull@localhost>
2015-03-14 12:50:17 +0200

    0.9.7-CT

--- src/xpm/xvm_techtree/__init__.py
+++ src/xpm/xvm_techtree/__init__.py
@@ -6,7 +6,7 @@
 XFW_MOD_VERSION    = "2.0.0"
 XFW_MOD_URL        = "http://www.modxvm.com/"
 XFW_MOD_UPDATE_URL = "http://www.modxvm.com/en/download-xvm/"
-XFW_GAME_VERSIONS  = ["0.9.6"]
+XFW_GAME_VERSIONS  = ["0.9.6","0.9.7"]
 
 #####################################################################
 
