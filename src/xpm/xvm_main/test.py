31e69dc75cb1481cef209675b1325d996a6edeee
sirmax2 <devnull@localhost>
2015-06-27 00:23:10 +0300

    fix

--- src/xpm/xvm_main/test.py
+++ src/xpm/xvm_main/test.py
@@ -17,28 +17,52 @@ def runTest(args):
     if cmd == 'battleResults':
         _showBattleResults(int(args[1][:-4]))
 
-###
+
+#############################
+# Imports
 
 import os
 import cPickle
 import traceback
+import BigWorld
 from logger import *
 
+
+#############################
 # BattleResults
 
 def _showBattleResults(arenaUniqueID):
+    from gui.shared import event_dispatcher as shared_events
+    shared_events.showMyBattleResults(arenaUniqueID)
+
+
+def BattleResultsCache_get(base, self, arenaUniqueID, callback):
     fileHandler = None
     try:
         filename = '{0}.dat'.format(arenaUniqueID)
-        if os.path.exists(filename):
+        if not os.path.exists(filename):
+            base(self, arenaUniqueID, callback)
+        else:
             fileHandler = open(filename, 'rb')
             version, battleResults = cPickle.load(fileHandler)
             if battleResults is not None:
-                from account_helpers import BattleResultsCache
-                from gui.shared import event_dispatcher as shared_events
-                shared_events.showBattleResultsFromData(BattleResultsCache.convertToFullForm(battleResults))
+                if callback is not None:
+                    import AccountCommands
+                    from account_helpers import BattleResultsCache
+                    callback(AccountCommands.RES_CACHE, BattleResultsCache.convertToFullForm(battleResults))
     except Exception, ex:
         err(traceback.format_exc())
+        base(self, arenaUniqueID, callback)
 
     if fileHandler is not None:
         fileHandler.close()
+
+
+#############################
+# Register events
+
+def _RegisterEvents():
+    log('_RegisterEvents')
+    from account_helpers import BattleResultsCache
+    OverrideMethod(BattleResultsCache.BattleResultsCache, 'get', BattleResultsCache_get)
+BigWorld.callback(0, _RegisterEvents)
