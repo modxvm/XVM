be5082d54360fdea4129eaff1d2ae894f256c6ae
sirmax2 <devnull@localhost>
2015-08-29 14:17:17 +0300

    update JPEXS decompiler with XVM patches: https://github.com/sirmax2/jpexs-decompiler-xvm

---
+++ build/bin/java/deswf.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ffdec="$(dirname $(realpath $(cygpath --unix $0)))/ffdec.sh"
+cfg=autoDeobfuscate=0,parallelSpeedUp=0
+fmt=script:as
+exp=script
+#exp=all
+
+for i in *.swf; do
+  echo $i
+  $ffdec -config $cfg -format $fmt -export $exp ${i/.swf/} $i >$i.log 2>$i.err
+done
+
+popd >/dev/null
