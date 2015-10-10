d8eb42a965e9abbecce38d8d032f59301fd5aef1
sirmax2 <devnull@localhost>
2015-10-10 21:00:34 +0300

    XVM-6.1.4.3-RELEASE

--- utils/test.sh
+++ utils/test.sh
@@ -5,7 +5,7 @@
 
 #SAMPLE_REPLAY=test.wotreplay
 #SAMPLE_REPLAY=test1.wotreplay
-#SAMPLE_REPLAY=test2.wotreplay
+SAMPLE_REPLAY=test2.wotreplay
 #SAMPLE_REPLAY=test3.wotreplay
 #SAMPLE_REPLAY=cw.wotreplay
 #SAMPLE_REPLAY=tk.wotreplay
