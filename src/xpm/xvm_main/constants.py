73b1429e7638787ed5de580cf71bae3f6558b158
sirmax2 <devnull@localhost>
2015-12-17 22:41:59 +0200

    XVM-6.2.0-RELEASE

--- src/xpm/xvm_main/constants.py
+++ src/xpm/xvm_main/constants.py
@@ -1,7 +1,7 @@
 """ XVM (c) www.modxvm.com 2013-2015 """
 
 class XVM(object):
-    XVM_VERSION    = '6.2.0-dev'
+    XVM_VERSION    = '6.2.0'
     WOT_VERSION    = '0.9.13'
     XVM_INTRO      = 'www.modxvm.com'
 
