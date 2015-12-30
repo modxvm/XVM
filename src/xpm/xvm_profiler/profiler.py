071a107fa9172f814df5d59204c8072095cfd201
sirmax2 <devnull@localhost>
2015-12-30 20:19:42 +0200

    xvm_profiler: fix

---
+++ src/xpm/xvm_profiler/profiler.py
@@ -0,0 +1,108 @@
+""" XVM (c) www.modxvm.com 2013-2015 """
+
+#####################################################################
+# imports
+
+import cProfile, pstats, StringIO
+
+import BigWorld
+import game
+from Avatar import PlayerAvatar
+from gui.Scaleform.Flash import Flash
+
+from xfw import *
+from xvm_main.python.logger import *
+
+import as2profiler
+
+
+#####################################################################
+# constants
+
+# for AS2
+class XVM_PROFILER_AS2COMMAND(object):
+    PROF_METHOD_START = "prof_method_start"
+    PROF_METHOD_END = "prof_method_end"
+
+
+#####################################################################
+# as2profiler
+
+_BATTLE_SWF = 'battle.swf'
+_VMM_SWF = 'VehicleMarkersManager.swf'
+_SWFS = [_BATTLE_SWF, _VMM_SWF]
+
+
+@registerEvent(Flash, '__init__')
+def FlashInit(self, swf, className='Flash', args=None, path=None):
+    self.swf = swf
+    if self.swf not in _SWFS:
+        return
+    self.addExternalCallback('xvm.cmd', lambda *args: onXvmCommand(self, *args))
+
+
+@registerEvent(Flash, 'beforeDelete')
+def FlashBeforeDelete(self):
+    if self.swf not in _SWFS:
+        return
+    # log("FlashBeforeDelete: " + self.swf)
+    self.removeExternalCallback('xvm.cmd')
+
+
+@registerEvent(game, 'fini')
+def fini():
+    showPythonResult()
+
+
+#####################################################################
+# onXvmCommand
+
+def onXvmCommand(proxy, id, cmd, *args):
+    try:
+        if cmd == XVM_PROFILER_AS2COMMAND.PROF_METHOD_START:
+            as2profiler.g_as2profiler.methodStart(args[0])
+        if cmd == XVM_PROFILER_AS2COMMAND.PROF_METHOD_END:
+            as2profiler.g_as2profiler.methodEnd(args[0])
+    except Exception, ex:
+        err(traceback.format_exc())
+
+
+#####################################################################
+# cProfile
+
+_pr = cProfile.Profile()
+_pr.enable()
+
+
+# on map load (battle loading)
+@registerEvent(PlayerAvatar, 'onEnterWorld')
+def PlayerAvatar_onEnterWorld(self, *args):
+    def en():
+        as2profiler.g_as2profiler.init()
+
+        global _pr
+        log('xvm_profiler enabled')
+        _pr.enable()
+    BigWorld.callback(10, en)
+
+
+# on map close
+@registerEvent(PlayerAvatar, 'onLeaveWorld')
+def PlayerAvatar_onLeaveWorld(self, *args):
+    as2profiler.g_as2profiler.showResult()
+
+    showPythonResult()
+
+
+_shown = False
+def showPythonResult():
+    global _shown
+    if not _shown:
+        _shown = True
+        global _pr
+        _pr.disable()
+        s = StringIO.StringIO()
+        sortby = 'cumulative'
+        p = pstats.Stats(_pr, stream=s).sort_stats(sortby)
+        p.print_stats('(xfw|xvm)', 20)
+        log(s.getvalue())
