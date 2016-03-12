7bfbaab84f018cd5af2d826947d1fb7ae3b6cf1b
sirmax2 <devnull@localhost>
2016-03-13 00:13:05 +0200

    %sup formatting

--- src/xpm/xvm_profile/__init__.py
+++ src/xpm/xvm_profile/__init__.py
@@ -139,7 +139,7 @@ def DetailedStatisticsUtils_getStatistics(base, targetData, isCurrentuser, layou
                 if xte > 0:
                     color = utils.getDynamicColorValue(constants.DYNAMIC_VALUE_TYPE.X, xte)
                     xteStr = 'XX' if xte == 100 else ('0' if xte < 10 else '') + str(xte)
-                    data = '<font color="#{}" size="12">({} {}%)</font> <font color="{}">{}</font>'.format(
+                    data = '<font color="#{}" size="12">({} {}%)</font>  <font color="{}">{}</font>'.format(
                         XFWCOLORS.UICOLOR_LABEL, l10n('better then'), xvm_scale.XvmScaleToSup(xte),
                         color, xteStr)
                     #log("xte={} color={}".format(xteStr, color))
