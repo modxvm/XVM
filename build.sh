b4b5de969fc65e1b74e92d5db4bd261774ea4ff4
sirmax2 <devnull@localhost>
2016-03-19 13:25:37 +0200

    typo

--- build.sh
+++ build.sh
@@ -200,7 +200,7 @@ copy_files(){
 
     # cp non-binary files
     mkdir -p "$XVMBUILD_REPOSITORY_PATH"/~output/"$XVMBUILD_WOT_VERSION"/audioww/
-    cp -f "$XVMBUILD_REPOSITORY_PATH"/release/audioww/*.bnk "$XVMBUILD_REPOSITORY_PATH"/~output/"$XVMBUILD_WOT_VERSION"/audioww/
+    cp -f "$XVMBUILD_REPOSITORY_PATH"/release/res/audioww/*.bnk "$XVMBUILD_REPOSITORY_PATH"/~output/"$XVMBUILD_WOT_VERSION"/audioww/
     cp -rf "$XVMBUILD_REPOSITORY_PATH"/release/* "$XVMBUILD_REPOSITORY_PATH"/~output/mods/shared_resources/xvm/
     mv "$XVMBUILD_REPOSITORY_PATH"/~output/mods/shared_resources/xvm/configs/* "$XVMBUILD_REPOSITORY_PATH"/~output/configs/xvm
     rm -rf "$XVMBUILD_REPOSITORY_PATH"/~output/mods/shared_resources/xvm/configs/
