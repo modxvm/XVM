6006c466009d3eb171ac8ef113a419f36f9283ed
monk143 <devnull@localhost>
2016-03-26 04:51:13 +0200

    total_hp panel phase 2

--- release/configs/default/battle.xc
+++ release/configs/default/battle.xc
@@ -54,9 +54,13 @@
     // true - show total HP of teams
     // true - показывать общее ХП команд
     "enabled": false,
-    // Color of HP, hex rgb
-    // Цвет ХП, hex rgb
-    "color": "FFFFFF",
+    // Color of HP per ratio ally/enemy team, hex rgb
+    // Цвет ХП в зависимости от отношения хп союзной и вражеской команд, hex rgb
+    "colors": {
+        "bad":     "FF0000",
+        "neutral": "FFFFFF",
+        "good":    "00FF00"
+    },
     // Font of HP (used fonts from: res/packages/misk.pkg/system/fonts/)
     // Шрифт ХП (используются шрифты из: res/packages/misk.pkg/system/fonts/)
     // default_medium.font, default_smaller.font, default_small.font, hpmp_panel.font, system_large.font, system_medium.font, system_small.font, system_tiny.font, verdana_medium.font, verdana_small.font
--- src/xpm/xvm_battle/fragCorrelationPanel.py
+++ src/xpm/xvm_battle/fragCorrelationPanel.py
@@ -10,8 +10,10 @@ import game
 from Avatar import PlayerAvatar
 from gui import g_guiResetters
 from gui.shared import g_eventBus
-import gui.Scaleform.daapi.view.battle.score_panel as score_panel
+from gui.Scaleform.daapi.view.battle import score_panel
+from gui.Scaleform.daapi.view.battle.markers import MarkersManager
 from gui.battle_control import g_sessionProvider
+from gui.battle_control.battle_constants import FEEDBACK_EVENT_ID
 
 from xfw import *
 
@@ -19,7 +21,6 @@ from xvm_main.python.constants import *
 from xvm_main.python.logger import *
 from xvm_main.python.xvm import Xvm as xvm_class
 from xvm_main.python import config
-from xvm_main.python import vehstate
 
 #####################################################################
 # initialization/finalization
@@ -38,7 +39,7 @@ def fini():
 # globals
 teams_vehicles = [{}, {}]
 teams_totalhp = [0, 0]
-hp_color = 'FFFFFF'
+hp_colors = {}
 
 #####################################################################
 # handlers
@@ -76,8 +77,13 @@ def populate_hp(*args, **kwargs):
     try:
         if not config.get('totalHP/enabled'):
             return
+        feedback = g_sessionProvider.getFeedback()
+        if feedback is not None:
+            feedback.onVehicleFeedbackReceived += onVehicleFeedbackReceived
         GUI.addRoot(hp_panel)
+        GUI.addRoot(hp_panel_shadow)
         hp_panel.visible = True
+        hp_panel_shadow.visible = True
         g_guiResetters.add(update_conf_hp)
     except Exception, ex:
         err(traceback.format_exc())
@@ -87,9 +93,14 @@ def destroy_hp(*args, **kwargs):
     try:
         if not config.get('totalHP/enabled'):
             return
+        feedback = g_sessionProvider.getFeedback()
+        if feedback is not None:
+            feedback.onVehicleFeedbackReceived -= onVehicleFeedbackReceived
         g_guiResetters.discard(update_conf_hp)
         hp_panel.visible = False
+        hp_panel_shadow.visible = False
         GUI.delRoot(hp_panel)
+        GUI.delRoot(hp_panel_shadow)
         teams_vehicles = [{}, {}]
         teams_totalhp = [0, 0]
     except Exception, ex:
@@ -101,6 +112,7 @@ def visibility_hp_panel(self, is_gui_visible):
         if not config.get('totalHP/enabled'):
             return
         hp_panel.visible = is_gui_visible
+        hp_panel_shadow.visible = is_gui_visible
     except Exception, ex:
         err(traceback.format_exc())
 
@@ -108,74 +120,138 @@ def update_conf_hp(*args, **kwargs):
     try:
         if not config.get('totalHP/enabled'):
             return
-        global hp_color
-        hp_color      = config.get('totalHP/color', 'FFFFFF')
-        hp_panel.font = config.get('totalHP/font', 'default_small.font')
-        vAlign        = config.get('totalHP/vAlign', 'top')
-        hAlign        = config.get('totalHP/hAlign', 'center')
-        x             = config.get('totalHP/x', 0)
-        y             = config.get('totalHP/y', 36)
-        res_x, res_y  = GUI.screenResolution()
+        hp_colors.update({'bad': 'FF0000', 'neutral': 'FFFFFF', 'good': '00FF00'})
+        hp_colors.update(config.get('totalHP/colors', {}))
+        hp_panel.font        = config.get('totalHP/font', 'default_small.font')
+        hp_panel_shadow.font = config.get('totalHP/font', 'default_small.font')
+        vAlign               = config.get('totalHP/vAlign', 'top')
+        hAlign               = config.get('totalHP/hAlign', 'center')
+        x                    = config.get('totalHP/x', 0)
+        y                    = config.get('totalHP/y', 36)
+        res_x, res_y         = GUI.screenResolution()
 
-        if hp_color[0] == '#':
-            hp_color = hp_color[1:]
+        for type, color in hp_colors.iteritems():
+            color = color[-6:]
+            hp_colors[type] = {'red': int(color[0:2], 16), 'green' : int(color[2:4], 16), 'blue': int(color[4:6], 16)}
 
         if vAlign == 'center':
             hp_panel.verticalAnchor = 'CENTER'
+            hp_panel_shadow.verticalAnchor = 'CENTER'
             y += res_y / 2
         elif vAlign == 'bottom':
             hp_panel.verticalAnchor = 'BOTTOM'
+            hp_panel_shadow.verticalAnchor = 'BOTTOM'
             y += res_y
         else:
             hp_panel.verticalAnchor = 'TOP'
+            hp_panel_shadow.verticalAnchor = 'TOP'
 
         if hAlign == 'center':
             hp_panel.horizontalAnchor = 'CENTER'
+            hp_panel_shadow.horizontalAnchor = 'CENTER'
             x += res_x / 2
         elif hAlign == 'right':
             hp_panel.horizontalAnchor = 'RIGHT'
+            hp_panel_shadow.horizontalAnchor = 'RIGHT'
             x += res_x
         else:
             hp_panel.horizontalAnchor = 'LEFT'
+            hp_panel_shadow.horizontalAnchor = 'LEFT'
             
         hp_panel.position = (x, y, 1)
+        hp_panel_shadow.position = (x + 1, y + 1, 1)
     except Exception, ex:
         err(traceback.format_exc())
 
-@registerEvent(xvm_class, 'doUpdateBattle')
-def update_hp(self, vID, *args, **kwargs):
+@registerEvent(xvm_class, '_onVehicleKilled')
+def xvm_onVehicleKilled(self, vID, *args, **kwargs):
+    try:
+        update_hp(vID, 0)
+    except Exception, ex:
+        err(traceback.format_exc())
+
+@registerEvent(PlayerAvatar, 'updateVehicleHealth')
+def setVehicleNewHealth(self, vehicleID, health, *args, **kwargs):
+    try:
+        update_hp(vehicleID, health)
+    except Exception, ex:
+        err(traceback.format_exc())
+
+@registerEvent(xvm_class, 'initBattleSwf')
+def initBattleSwf(self, *args, **kwargs):
+    try:
+        for vID, vData in BigWorld.player().arena.vehicles.iteritems():
+            update_hp(vID, vData['vehicleType'].maxHealth)
+    except Exception, ex:
+        err(traceback.format_exc())
+
+@registerEvent(MarkersManager, 'addVehicleMarker')
+def addVehicleMarker(self, vProxy, *args, **kwargs):
+    try:
+        update_hp(vProxy.id, vProxy.health)
+    except Exception, ex:
+        err(traceback.format_exc())
+
+def onVehicleFeedbackReceived(eventID, vehicleID, value, *args, **kwargs):
+    try:
+        if eventID == FEEDBACK_EVENT_ID.VEHICLE_HEALTH:
+            update_hp(vehicleID, value[0])
+    except Exception, ex:
+        err(traceback.format_exc())
+
+def color_gradient(color1, color2, ratio):
+    try:
+        ratio_comp = 1.0 - ratio
+        return '%0.2X%0.2X%0.2X' % (
+                color1['red'] * ratio + color2['red'] * ratio_comp,
+                color1['green'] * ratio + color2['green'] * ratio_comp,
+                color1['blue'] * ratio + color2['blue'] * ratio_comp,
+                )
+    except Exception, ex:
+        err(traceback.format_exc())
+        return 'FFFFFF'
+
+def update_hp(vID, hp, *args, **kwargs):
     try:
         if not config.get('totalHP/enabled'):
             return
-        state = vehstate.getVehicleStateData(vID)
-        if state is None:
-            return
-        team = state['team'] - 1
-        if state['dead']:
-            hp = 0
-        elif state['curHealth'] is not None:
-            hp = state['curHealth']
+
+        if BigWorld.player().team == BigWorld.player().arena.vehicles[vID]['team']:
+            team = 0
         else:
-            veh_descr = BigWorld.player().arena.vehicles.get(vID, {}).get('vehicleType')
-            if not veh_descr:
-                return
-            hp = veh_descr.maxHealth
+            team = 1
 
         teams_vehicles[team][vID] = hp
         teams_totalhp[team] = sum(teams_vehicles[team].values())
     
         if teams_totalhp[0] < teams_totalhp[1]:
+            ratio = max(min(2.0 * teams_totalhp[0] / teams_totalhp[1] - 0.9, 1), 0)
+            color = color_gradient(hp_colors['neutral'], hp_colors['bad'], ratio)
             sign = '<'
         elif teams_totalhp[0] > teams_totalhp[1]:
+            ratio = max(min(2.0 * teams_totalhp[1] / teams_totalhp[0] - 0.9, 1), 0)
+            color = color_gradient(hp_colors['neutral'], hp_colors['good'], ratio)
             sign = '>'
         else:
+            color = color_gradient(hp_colors['neutral'], hp_colors['good'], 1)
             sign = '='
-        hp_panel.text = '\c%sFF;%s %s %s' % (hp_color, teams_totalhp[0], sign, teams_totalhp[1])
+        hp_panel.text = '\c%sFF;%s %s %s' % (color, teams_totalhp[0], sign, teams_totalhp[1])
+        hp_panel_shadow.text = '\c000000FF;%s %s %s' % (teams_totalhp[0], sign, teams_totalhp[1])
     except Exception, ex:
         err(traceback.format_exc())
 
-hp_panel = GUI.Text('')
-hp_panel.colourFormatting = True
-hp_panel.horizontalPositionMode = 'PIXEL'
-hp_panel.verticalPositionMode = 'PIXEL'
-hp_panel.text = ''
+try:
+    hp_panel = GUI.Text('')
+    hp_panel.colourFormatting = True
+    hp_panel.horizontalPositionMode = 'PIXEL'
+    hp_panel.verticalPositionMode = 'PIXEL'
+    hp_panel.text = ''
+
+    hp_panel_shadow = GUI.Text('')
+    hp_panel_shadow.colourFormatting = True
+    hp_panel_shadow.horizontalPositionMode = 'PIXEL'
+    hp_panel_shadow.verticalPositionMode = 'PIXEL'
+    hp_panel_shadow.text = ''
+
+except Exception, ex:
+    err(traceback.format_exc())
