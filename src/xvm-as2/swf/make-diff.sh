d95d6272a17d5e358bc312082149ee523c0684c5
Mikhail Paulyshka <me@mixaill.tk>
2016-03-31 00:54:01 +0300

    [BUILD] fix AS2 make-diff.sh
    add ability to use embedded swfmill

--- src/xvm-as2/swf/make-diff.sh
+++ src/xvm-as2/swf/make-diff.sh
@@ -12,13 +12,31 @@ patch_swfs="
     VehicleMarkersManager.swf
 "
 
-for swf in $patch_swfs; do
-    swf=${swf%.*}
-    if [ ! -f ${swf}.xml ]; then
-      echo ${swf}.swf
-      swfmill swf2xml orig/${swf}.swf orig/${swf}.xml
-      swfmill swf2xml ${swf}.swf ${swf}.xml
-      diff -u2 -I "<StackDouble value=" orig/${swf}.xml ${swf}.xml > ${swf}.xml.patch
-      rm orig/${swf}.xml ${swf}.xml
-    fi
-done
+currentdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$currentdir"
+source ../../../build/library.sh
+
+extend_path()
+{
+  PATH="$currentdir"/../../../build/bin/"$OS"_"$arch"/:$PATH
+}
+
+make_diff_as2(){
+  for swf in $patch_swfs; do
+      swf=${swf%.*}
+      if [ ! -f ${swf}.xml ]; then
+        echo ${swf}.swf
+        swfmill swf2xml orig/${swf}.swf orig/${swf}.xml
+        swfmill swf2xml ${swf}.swf ${swf}.xml
+        diff -u2 -I "<StackDouble value=" orig/${swf}.xml ${swf}.xml > ${swf}.xml.patch
+        rm orig/${swf}.xml ${swf}.xml
+      fi
+  done
+}
+
+detect_os
+detect_arch
+extend_path
+detect_swfmill
+
+make_diff_as2
