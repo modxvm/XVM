449006e2195aa98b4bf41f0509a9cb07215c14c9
sirmax2 <devnull@localhost>
2016-06-09 15:41:34 +0300

    py_macro: add math functions

--- release/configs/py_macro/math.py
+++ release/configs/py_macro/math.py
@@ -1,3 +1,24 @@
+﻿# Addition. Сложение.
+@xvm.export('math.sum')
+def sum(a, b):
+    return a + b
+
+# Subtraction. Вычитание.
+@xvm.export('math.sub')
+def razn(a, b):
+    return a - b
+
+# Multiplication. Умножение.
+@xvm.export('math.mul')
+def mul(a, b):
+    return a * b
+
+# Division. Деление.
+@xvm.export('math.div')
+def div(a, b):
+    return a / b
+
+# Raise to power. Возведение в степень.
 @xvm.export('math.pow')
 def pow(a, n):
     return a ** n
