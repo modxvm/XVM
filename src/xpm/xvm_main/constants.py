10e985a03a6b09707f4bac4a63df862daf94e3c1
Mikhail Paulyshka <me@mixaill.tk>
2016-07-07 12:23:23 +0300

    [RELEASE] Bump vesion

--- src/xpm/xvm_main/constants.py
+++ src/xpm/xvm_main/constants.py
@@ -1,7 +1,7 @@
 """ XVM (c) www.modxvm.com 2013-2016 """
 
 class XVM(object):
-    XVM_VERSION    = '6.3.2'
+    XVM_VERSION    = '6.3.2.1'
     WOT_VERSION    = '0.9.15.0.1'
     XVM_INTRO      = 'www.modxvm.com'
 
