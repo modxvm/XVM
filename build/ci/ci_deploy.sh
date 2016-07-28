7f36cdd20880ad2f34846cc052eb1bfb3f8d4613
Mikhail Paulyshka <me@mixaill.tk>
2016-07-28 13:00:38 +0300

    [CI] Fix deploy

--- build/ci/ci_deploy.sh
+++ build/ci/ci_deploy.sh
@@ -5,21 +5,21 @@
 
 CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$CURRENT_PATH"
-export XVMBUILD_REPOSITORY_PATH="$CURRENT_PATH/../.."
+export XVMBUILD_ROOT_PATH="$CURRENT_PATH/../.."
 
 source /var/xvm/ci_config.sh
-source "$XVMBUILD_REPOSITORY_PATH/build/xvm-build.conf"
-source "$XVMBUILD_REPOSITORY_PATH/build/library.sh"
+source "$XVMBUILD_ROOT_PATH/build/xvm-build.conf"
+source "$XVMBUILD_ROOT_PATH/build/library.sh"
 
 pack_xvm(){
     echo ""
     echo "Packing build"
 
-    echo "$XVMBUILD_XVM_REVISION" >> "$XVMBUILD_REPOSITORY_PATH"/~output/"$XVMBUILD_XVM_REVISION"
-    echo "$XVMBUILD_XVM_HASH" >> "$XVMBUILD_REPOSITORY_PATH"/~output/"$XVMBUILD_XVM_REVISION"
-    echo "$XVMBUILD_XVM_BRANCH" >> "$XVMBUILD_REPOSITORY_PATH"/~output/"$XVMBUILD_XVM_REVISION"
+    echo "$XVMBUILD_XVM_REVISION" >> "$XVMBUILD_ROOT_PATH"/~output/"$XVMBUILD_XVM_REVISION"
+    echo "$XVMBUILD_XVM_HASH" >> "$XVMBUILD_ROOT_PATH"/~output/"$XVMBUILD_XVM_REVISION"
+    echo "$XVMBUILD_XVM_BRANCH" >> "$XVMBUILD_ROOT_PATH"/~output/"$XVMBUILD_XVM_REVISION"
 
-    pushd "$XVMBUILD_REPOSITORY_PATH"/~output/ > /dev/null
+    pushd "$XVMBUILD_ROOT_PATH"/~output/ > /dev/null
     zip -9 -r -q "$XVMBUILD_XVM_REVISION"_"$XVMBUILD_XVM_HASH"_xvm.zip ./
     popd > /dev/null
 }
@@ -28,11 +28,11 @@ pack_xfw(){
     echo ""
     echo "Packing XFW"
 
-    echo "$XVMBUILD_XFW_REVISION" >> "$XVMBUILD_REPOSITORY_PATH"/src/xfw/~output/"$XVMBUILD_XFW_REVISION"
-    echo "$XVMBUILD_XFW_HASH" >> "$XVMBUILD_REPOSITORY_PATH"/src/xfw/~output/"$XVMBUILD_XFW_REVISION"
-    echo "$XVMBUILD_XFW_BRANCH" >> "$XVMBUILD_REPOSITORY_PATH"/src/xfw/~output/"$XVMBUILD_XFW_REVISION"
+    echo "$XVMBUILD_XFW_REVISION" >> "$XVMBUILD_ROOT_PATH"/src/xfw/~output/"$XVMBUILD_XFW_REVISION"
+    echo "$XVMBUILD_XFW_HASH" >> "$XVMBUILD_ROOT_PATH"/src/xfw/~output/"$XVMBUILD_XFW_REVISION"
+    echo "$XVMBUILD_XFW_BRANCH" >> "$XVMBUILD_ROOT_PATH"/src/xfw/~output/"$XVMBUILD_XFW_REVISION"
 
-    pushd "$XVMBUILD_REPOSITORY_PATH"/src/xfw/~output/ > /dev/null
+    pushd "$XVMBUILD_ROOT_PATH"/src/xfw/~output/ > /dev/null
     zip -9 -r -q "$XVMBUILD_XFW_REVISION"_"$XVMBUILD_XFW_HASH"_xfw.zip ./
     rm "$XVMBUILD_XFW_REVISION"
     popd > /dev/null
@@ -45,8 +45,8 @@ deploy_xvm(){
 
     mkdir -p "$XVMBUILD_OUTPUT_PATH"/
 
-    mv -f "$XVMBUILD_REPOSITORY_PATH"/src/xfw/~output/"$XVMBUILD_XFW_REVISION"_"$XVMBUILD_XFW_HASH"_xfw.zip "$XVMBUILD_OUTPUT_PATH"/
-    mv -f "$XVMBUILD_REPOSITORY_PATH"/~output/"$XVMBUILD_XVM_REVISION"_"$XVMBUILD_XVM_HASH"_xvm.zip "$XVMBUILD_OUTPUT_PATH"/
+    mv -f "$XVMBUILD_ROOT_PATH"/src/xfw/~output/"$XVMBUILD_XFW_REVISION"_"$XVMBUILD_XFW_HASH"_xfw.zip "$XVMBUILD_OUTPUT_PATH"/
+    mv -f "$XVMBUILD_ROOT_PATH"/~output/"$XVMBUILD_XVM_REVISION"_"$XVMBUILD_XVM_HASH"_xvm.zip "$XVMBUILD_OUTPUT_PATH"/
 
     cp -f "$XVMBUILD_OUTPUT_PATH"/"$XVMBUILD_XVM_REVISION"_"$XVMBUILD_XVM_HASH"_xvm.zip "$XVMBUILD_OUTPUT_PATH"/latest_xvm.zip
     cp -f "$XVMBUILD_OUTPUT_PATH"/"$XVMBUILD_XFW_REVISION"_"$XVMBUILD_XFW_HASH"_xfw.zip "$XVMBUILD_OUTPUT_PATH"/latest_xfw.zip
@@ -64,8 +64,8 @@ deploy_xvm(){
 
 deploy_xvminst()
 {
-    mv -f "$XVMBUILD_REPOSITORY_PATH"/xvminst/output/"$XVMBUILD_XVM_BRANCH"/"$XVMBUILD_XVM_REVISION"_"$XVMBUILD_XVM_BRANCH"_xvm.exe "$XVMBUILD_OUTPUT_PATH"/
-    mv -f "$XVMBUILD_REPOSITORY_PATH"/xvminst/output/"$XVMBUILD_XVM_BRANCH"/latest_"$XVMBUILD_XVM_BRANCH"_xvm.exe "$XVMBUILD_OUTPUT_PATH"/
+    mv -f "$XVMBUILD_ROOT_PATH"/src/installer/output/"$XVMBUILD_XVM_BRANCH"/"$XVMBUILD_XVM_REVISION"_"$XVMBUILD_XVM_BRANCH"_xvm.exe "$XVMBUILD_OUTPUT_PATH"/
+    mv -f "$XVMBUILD_ROOT_PATH"/src/installer/output/"$XVMBUILD_XVM_BRANCH"/latest_"$XVMBUILD_XVM_BRANCH"_xvm.exe "$XVMBUILD_OUTPUT_PATH"/
 }
 
 load_repositorystats
