4820d78dfd377a147025038369abc29a8a42e23d
sirmax2 <devnull@localhost>
2016-08-11 17:06:16 +0300

    XVM-6.4.2-dev

--- src/xpm/xvm_main/consts.py
+++ src/xpm/xvm_main/consts.py
@@ -1,7 +1,7 @@
 """ XVM (c) www.modxvm.com 2013-2016 """
 
 class XVM(object):
-    XVM_VERSION    = '6.4.1.1'
+    XVM_VERSION    = '6.4.2-dev'
     WOT_VERSION    = '0.9.15.1.1'
     XVM_INTRO      = 'www.modxvm.com'
 
