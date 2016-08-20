e4d436fbce1ad1c517ac10164ac4824c3f0b30b7
sirmax2 <max@modxvm.com>
2016-08-20 11:28:41 +0300

    XVM-6.4.2.1-RELEASE

--- src/xpm/xvm_main/consts.py
+++ src/xpm/xvm_main/consts.py
@@ -1,7 +1,7 @@
 """ XVM (c) www.modxvm.com 2013-2016 """
 
 class XVM(object):
-    XVM_VERSION    = '6.4.3-dev'
+    XVM_VERSION    = '6.4.2.1'
     WOT_VERSION    = '0.9.15.1.1'
     XVM_INTRO      = 'www.modxvm.com'
 
