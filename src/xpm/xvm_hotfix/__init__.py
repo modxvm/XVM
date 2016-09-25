2e5ca6f26c42f5df7993846705d42335e1d4f58d
sirmax2 <max@modxvm.com>
2016-09-25 19:52:56 +0300

    WoT 9.16 (CT)

--- src/xpm/xvm_hotfix/__init__.py
+++ src/xpm/xvm_hotfix/__init__.py
@@ -16,23 +16,7 @@ XFW_MOD_INFO = {
 #####################################################################
 #imports
 
-import BigWorld
-from AvatarInputHandler import AvatarInputHandler
-from gui.Scaleform.CursorDelegator import g_cursorDelegator
-from gui.Scaleform.framework.entities.abstract.ContainerManagerMeta import ContainerManagerMeta
-
 from xfw import *
-
 from xvm_main.python.logger import *
 
-
 #####################################################################
-
-import update_arenas_data
-update_arenas_data.run()
-
-# cursor bug 0.9.15.1
-@overrideMethod(AvatarInputHandler, 'setForcedGuiControlMode')
-def _AvatarInputHandler_setForcedGuiControlMode(base, self, flags):
-    base(self, flags)
-    g_cursorDelegator._CursorDelegator__activated = self._AvatarInputHandler__isDetached
