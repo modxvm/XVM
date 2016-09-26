88a61df745e97ba54f980d9cf9a37e198d216a46
Mikhail Paulyshka <me@mixaill.tk>
2016-09-26 03:06:38 +0300

    xvm-native/ping: fix python c functions signatures.

--- src/xvm-native/src/ping/pythonWrapper.c
+++ src/xvm-native/src/ping/pythonWrapper.c
@@ -25,9 +25,9 @@ void pythonWrapperInit(void)
     endpos = (DWORD)GetModuleSize(lpFilename);
     curpos = startpos;
 
-    wrp_PyArg_ParseTuple_func = (wrp_PyArg_ParseTuple_typedef*)(FindFunction(startpos, endpos, &curpos, "\x55\x8B\xEC\x51\x8D\x45\x10\x89\x45\xFC\x6A\x00\x8D\x45\xFC\x50","xxxxxxxxxxxxxxxx"));
+    wrp_PyArg_ParseTuple_func = (wrp_PyArg_ParseTuple_typedef*)(FindFunction(startpos, endpos, &curpos, "\x55\x8B\xEC\x51\x8D\x45\x10\x89\x45\xFC\x8D\x45\xFC\x6A\x00","xxxxxxxxxxxxxxx"));
     wrp_Py_BuildValue_func = (wrp_Py_BuildValue_typedef*)(FindFunction(startpos, endpos, &curpos, "\x55\x8B\xEC\x6A\x00\x8D\x45\x0C\x50\xFF\x75\x08\xE8","xxxxxxxxxxxxx"));
-    wrp_Py_InitModule4_func = (wrp_Py_InitModule4_typedef*)(FindFunction(startpos, endpos, &curpos, "\x55\x8B\xEC\x81\xEC\x14\x02\x00\x00\xA1\x00\x00\x00\x00\x33\xC5\x89\x45\xFC\x8B\x45\x10\x53\x8B\x5D\x0C\x89\x85\xF4\xFD\xFF\xFF","xxxxxxxxxx????xxxxxxxxxxxxxxxxxx"));
+    wrp_Py_InitModule4_func = (wrp_Py_InitModule4_typedef*)(FindFunction(startpos, endpos, &curpos, "\x55\x8B\xEC\x81\xEC\x14\x02\x00\x00\xA1\x00\x00\x00\x00\x33\xC5\x89\x45\xFC\x8B\x45\x10\x53\x8B\x5D\x0C\x89\x85\x00\xFD\xFF\xFF","xxxxxxxxxx????xxxxxxxxxxxxxx?xxx"));
     wrp_PyEval_RestoreThread_func = (wrp_PyEval_RestoreThread_typedef*)(FindFunction(startpos, endpos, &curpos, "\x55\x8B\xEC\x83\x7D\x08\x00\x75\x0D\x68\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x83\xC4\x04\x83\x3D","xxxxxxxxxx????x????xxxxx"));
     wrp_PyEval_SaveThread_func = (wrp_PyEval_SaveThread_typedef*)(FindFunction(startpos, endpos, &curpos, "\x56\x6A\x00\xE8\x00\x00\x00\x00\x8B\xF0\x83\xC4\x04\x85\xF6\x75\x0D","xxxx????xxxxxxxxx"));
 }
