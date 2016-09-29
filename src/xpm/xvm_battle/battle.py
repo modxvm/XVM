f777423b2bff0034ee4cf850872736e56abe919b
sirmax2 <max@modxvm.com>
2016-09-29 21:30:11 +0300

    fix

--- src/xpm/xvm_battle/battle.py
+++ src/xpm/xvm_battle/battle.py
@@ -149,9 +149,9 @@ def _DynSquadFunctional_updateVehiclesInfo(self, updated, arenaDP):
         if BigWorld.player().arena.guiType == constants.ARENA_GUI_TYPE.RANDOM:
             for flags, vo in updated:
                 if flags & INVALIDATE_OP.PREBATTLE_CHANGED and vo.squadIndex > 0:
-                    for index, (vInfoVO, vStatsVO, viStatsVO) in enumerate(arenaDP.getTeamIterator(vo.team)):
-                        if vInfoVO.squadIndex > 0:
-                            g_battle.updatePlayerState(vehicleID, INV.SQUAD_INDEX) # | INV.PLAYER_STATUS
+                    for vInfoVO in arenaDP.getVehiclesInfoIterator():
+                        if vInfoVO.team == vo.team and vInfoVO.squadIndex == vo.squadIndex:
+                            g_battle.updatePlayerState(vInfoVO.vehicleID, INV.SQUAD_INDEX) # | INV.PLAYER_STATUS
     except Exception, ex:
         err(traceback.format_exc())
 
