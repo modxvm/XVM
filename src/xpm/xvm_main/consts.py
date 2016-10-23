f79aa904317028d4dc51716f5202576a2dbe1661
sirmax2 <max@modxvm.com>
2016-10-23 18:34:06 +0300

    fixed "crewMaxPerksCount" option

--- src/xpm/xvm_main/consts.py
+++ src/xpm/xvm_main/consts.py
@@ -1,7 +1,7 @@
 """ XVM (c) www.modxvm.com 2013-2016 """
 
 class XVM(object):
-    XVM_VERSION    = '6.4.8-dev'
+    XVM_VERSION    = '6.4.7.1'
     WOT_VERSION    = '0.9.16'
     XVM_INTRO      = 'www.modxvm.com'
 
