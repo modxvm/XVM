b43ed00ba911dcb4ca3e53b97bd4dc07afd1a835
Mikhail Paulyshka <mixail@modxvm.com>
2016-11-13 13:08:57 +0300

    xvm_sounds: fix bank load/unload problem add some documentation to code add bank_add()/bank_remove() API calls

--- src/xpm/xvm_sounds/bankManager.py
+++ src/xpm/xvm_sounds/bankManager.py
@@ -20,50 +20,66 @@ from xvm_main.python.utils import fixXvmPath
  
 class BankManager(object):
     def __init__(self):
+        """
+        BankManager initialization
+        """
         self.XVMNativeSounds = imp.load_dynamic('XVMNativeSounds','./res_mods/mods/packages/xvm_sounds/native/XVMNativeSounds.pyd')
 
+        self.banks_battle=set()
         extraBanksBattle = config.get('sounds/soundBanks/battle')
         if extraBanksBattle:
-            self.banks_battle = set([x.strip() for x in extraBanksBattle if x and x.strip()])
-        else:
-            self.banks_battle=set()
-
+            for bank in extraBanksBattle:
+                self.bank_add(bank.strip(),True,False)
+            
+        self.banks_hangar=set()
         extraBanksHangar = config.get('sounds/soundBanks/hangar')
         if extraBanksHangar:
-            self.banks_hangar = set([x.strip() for x in extraBanksHangar if x and x.strip()])
-        else:
-            self.banks_hangar=set()
+            for bank in extraBanksHangar:
+                self.bank_add(bank.strip(),False,True)
 
         self.banks_loaded=dict()
 
     def __fini__(self):
+        """
+        BankManager finalization
+        """
         for value in self.banks_loaded.itervalues():
             self.bank_unload(value)
 
-    def bank_load(self, bankName):
-        #log('BankManager/BankLoad: bankName=%s' % bankName)
-        bankName=fixXvmPath(bankName,PATH.GENERAL_MODS_DIR + '/audioww/').lower()
 
-        try:
-            bankID = self.XVMNativeSounds.bank_load(bankName)
-            if bankID:
-                self.banks_loaded[bankName]=bankID
-        except Exception, e:
-            warn(e)
+    def bank_add(self,bankName,toBattle,toHangar):
+        """
+        Add bank to loading list.
+        Use reload() to perform bank load.
+        """
+        if toBattle:
+            self.banks_battle.add(self._normalize_path(bankName))
 
-    def bank_unload(self, bankName):
-        #log('BankManager/BankUnload: bankName=%s' % bankName)
-        bankName=fixXvmPath(bankName,PATH.GENERAL_MODS_DIR + '/audioww/').lower()
+        if toHangar:
+            self.banks_hangar.add(self._normalize_path(bankName))
 
-        try:
-            bankID = self.banks_loaded.pop(bankName)
-            if bankID:
-                self.XVMNativeSounds.bank_unload(bankID)
-        except Exception, e:
-            warn(e)
+    def bank_remove(self,bankName,fromBattle,fromHangar):
+        """
+        Remove bank from loading list.
+        Use reload() to perform bank unload.
+        """
+        if fromBattle:
+            if self._normalize_name(bankName) in self.banks_battle:
+                self.banks_battle.remove(self._normalize_name(bankName))
+
+        if fromHangar:
+            if self._normalize_name(bankName) in self.banks_hangar:
+                self.banks_hangar.remove(self._normalize_name(bankName))
 
     def reload(self, loadBattleBanks, loadHangarBanks):
-        #log('BankManager/Reload: battle=%s, hangar=%s' % (loadBattleBanks, loadHangarBanks) )
+        """
+        Perform banks load and unload from banks_battle and/or banks_hangar
+
+        loadBattleBanks -- True to load banks from banks_battle
+        loadHangarBanks -- True to load banks from banks_hangar
+        """
+        #log('BankManager/Reload: battle=%s, hangar=%s' % (loadBattleBanks, loadHangarBanks))
+
         banksToLoad = set()
         banksToUnload = set()
 
@@ -78,19 +94,64 @@ class BankManager(object):
                 banksToUnload.add(key)
 
         for x in banksToUnload:
-            self.bank_unload(x)
+            self._bank_unload(x)
 
         for x in banksToLoad:
             if x not in self.banks_loaded:
-                self.bank_load(x)
+                self._bank_load(x)
+
+    def _bank_load(self, bankPath):
+        """
+        Load bank using WWise Native API.
+        Do not use it directly. Add bank with bank_add() and then reload() instead.
+
+        bankPath -- path relative to game root (WorldOfTanks.exe directory)
+        """
+        #log('BankManager/BankLoad: bankPath=%s' % bankPath)
+
+        try:
+            bankID = self.XVMNativeSounds.bank_load(bankPath)
+            if bankID:
+                self.banks_loaded[bankPath]=bankID
+        except Exception, e:
+            warn(e)
+
+    def _bank_unload(self, bankPath):
+        """
+        Unload bank using WWise Native API. 
+        Do not use it directly. Remove bank with bank_remove() and then reload() instead.
+
+        bankPath -- path relative to game root (WorldOfTanks.exe directory)
+        """
+        #log('BankManager/BankUnload: bankPath=%s' % bankPath)
+
+        try:
+            bankID = self.banks_loaded.pop(bankPath)
+            if bankID:
+                self.XVMNativeSounds.bank_unload(bankID)
+        except Exception, e:
+            warn(e)
+
+    def _normalize_path(self, path):
+        """
+        Normalize path to sound bank:
+
+        cfg://* -> /res_mods/configs/xvm/*
+        xvm://* -> /res_mods/mods/shared_resources/xvm/*
+        *       -> /res_mods/x.x.x/audioww/*
+        """
+        return fixXvmPath(path,PATH.GENERAL_MODS_DIR + '/audioww/').lower()
 
 #####################################################################
-# handlers
+# initialization
 
 g_xvmBankManager = None
 if config.get('sounds/enabled'):
     g_xvmBankManager = BankManager()
     g_xvmBankManager.reload(False,True)
+
+#####################################################################
+# handlers
 
 @overrideMethod(PlayerAvatar, 'onBecomePlayer')
 def _PlayerAvatar_onBecomePlayer(base, self):
