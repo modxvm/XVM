60176ed7667c6fe94151ea70020bfe1839ac5b1e
sirmax2 <max@modxvm.com>
2016-11-20 19:34:42 +0200

    added option "hangar"/"showNotificationsCounter"

---
+++ src/xpm/xvm_hangar/svcmsg.py
@@ -0,0 +1,16 @@
+﻿import traceback
+
+import BigWorld
+from gui.Scaleform.daapi.view.lobby.messengerBar.NotificationListButton import NotificationListButton
+
+from xfw import *
+import xvm_main.python.config as config
+from xvm_main.python.logger import *
+
+###
+
+@overrideMethod(NotificationListButton, 'as_setStateS')
+def _NotificationListButton_as_setStateS(base, self, isBlinking, counterValue):
+    if not config.get('hangar/showNotificationsCounter', True):
+        counterValue = ''
+    base(self, isBlinking, counterValue)
