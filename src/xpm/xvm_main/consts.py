f2d975794010af845f6a239dba90bb4cf9df88c7
Mikhail Paulyshka <me@mixaill.tk>
2017-01-24 13:31:38 +0300

    XVM-6.5.4

--- src/xpm/xvm_main/consts.py
+++ src/xpm/xvm_main/consts.py
@@ -1,7 +1,7 @@
 """ XVM (c) www.modxvm.com 2013-2017 """
 
 class XVM(object):
-    XVM_VERSION    = '6.5.4-dev'
+    XVM_VERSION    = '6.5.4'
     WOT_VERSION    = '0.9.17.0.3'
     XVM_INTRO      = 'www.modxvm.com'
 
