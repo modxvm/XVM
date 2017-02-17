bf7a1b0cf7f488a2979ad6111735da701ae2c5d7
Mikhail Paulyshka <me@mixaill.tk>
2017-02-17 03:21:30 +0300

    xvm_sounds/bankmanager: move WWise bank loader to XFW xvm_main/utils: move fixPath and fixXvmPath to XFW

--- src/xvm-native/packages/xvm_sounds/build_msvs.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rmdir /S /Q build
-mkdir build
-cd build
-
-cmake .. -G "Visual Studio 14 2015" -T "v140_xp"
-cmake --build . --config RelWithDebInfo
-cmake --build . --target INSTALL --config RelWithDebInfo
