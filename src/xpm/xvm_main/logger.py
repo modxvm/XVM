658fc46e8c4e944246df0d3f2d02e84588cf9b4c
sirmax2 <max@modxvm.com>
2017-02-24 23:39:16 +0200

    use pprint for object logging

--- src/xpm/xvm_main/logger.py
+++ src/xpm/xvm_main/logger.py
@@ -1,5 +1,7 @@
 """ XVM (c) www.modxvm.com 2013-2017 """
 
+import pprint
+
 from xfw import *
 from xfw import swf
 from xfw.constants import PATH
@@ -7,18 +9,26 @@ from xfw.constants import PATH
 if swf.g_xvmlogger is None:
     swf.g_xvmlogger = Logger(PATH.XVM_LOG_FILE_NAME)
 
-def log(s, prefix=''):
-    swf.g_xvmlogger.add('%s%s' % (prefix, str(s)))
+def log(s, prefix='', *args, **kwargs):
+    if not isinstance(s, basestring):
+        s = pprint.pformat(s, *args, **kwargs)
+    swf.g_xvmlogger.add('%s%s' % (prefix, s))
 
-def err(s):
+def err(s, *args, **kwargs):
+    if not isinstance(s, basestring):
+        s = pprint.pformat(s, *args, **kwargs)
     swf.g_xvmlogger.error(s)
 
-def warn(s):
+def warn(s, *args, **kwargs):
+    if not isinstance(s, basestring):
+        s = pprint.pformat(s, *args, **kwargs)
     swf.g_xvmlogger.warning(s)
 
-def debug(s):
+def debug(s, *args, **kwargs):
+    if not isinstance(s, basestring):
+        s = pprint.pformat(s, *args, **kwargs)
     swf.g_xvmlogger.debug(s)
 
-def trace(s):
+def trace(s, *args, **kwargs):
     if IS_DEVELOPMENT:
-        log(s, '[TRACE] >> ')
+        log(s, '[TRACE] >> ', *args, **kwargs)
