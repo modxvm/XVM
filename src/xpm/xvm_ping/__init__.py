5e97b0c6a8a7a713ab83b35588270fcdc35f2382
Mikhail Paulyshka <me@mixaill.tk>
2017-03-31 02:22:54 +0300

    xpm: bump wot version

--- src/xpm/xvm_ping/__init__.py
+++ src/xpm/xvm_ping/__init__.py
@@ -5,10 +5,10 @@
 
 XFW_MOD_INFO = {
     # mandatory
-    'VERSION':       '0.9.17.1',
+    'VERSION':       '0.9.18',
     'URL':           'http://www.modxvm.com/',
     'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
-    'GAME_VERSIONS': ['0.9.17.1'],
+    'GAME_VERSIONS': ['0.9.18'],
     # optional
 }
 
