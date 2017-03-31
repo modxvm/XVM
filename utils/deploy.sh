7dae00a78a4a3cb02ff75b9beb819ca16ba8da46
sirmax2 <max@modxvm.com>
2017-04-01 01:46:58 +0300

    update deploy.sh

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -19,58 +19,39 @@ check_config()
 {
   [ "$WOT_PATH" != "" ] || err "WOT_PATH is not set in the build/xvm-build.conf"
   [ -d "$WOT_PATH" ] || err "WOT_PATH is invalid in the build/xvm-build.conf  WOT_PATH=$WOT_PATH"
-  [ "$TARGET_VERSION" != "" ] || err "TARGET_VERSION is not set in the build/xvm-build.conf"
-  [ -d "$WOT_PATH/res_mods/$TARGET_VERSION" ] || {
-    err "TARGET_VERSION is invalid in the build/xvm-build.conf  TARGET_VERSION=$TARGET_VERSION"
+  [ "$XVMBUILD_WOT_VERSION" != "" ] || err "XVMBUILD_WOT_VERSION is not set in the build/xvm-build.conf"
+  [ -d "$WOT_PATH/mods/$XVMBUILD_WOT_VERSION" -a -d "$WOT_PATH/res_mods/$XVMBUILD_WOT_VERSION" ] || {
+    err "XVMBUILD_WOT_VERSION is invalid in the build/xvm-build.conf  XVMBUILD_WOT_VERSION=$XVMBUILD_WOT_VERSION"
   }
 }
 
-clear()
-{
-  rm -rf "$WOT_PATH/res_mods/configs/xvm/py_macro/xvm" || err "make_dirs"
-  rm -rf "$WOT_PATH/res_mods/mods/packages" || err "clear"
-  rm -rf "$WOT_PATH/res_mods/mods/xfw" || err "clear"
-  rm -f "$WOT_PATH/res_mods/$TARGET_VERSION/gui/flash/lobby.swf" || err "clear"
-  rm -rf "$WOT_PATH/res_mods/$TARGET_VERSION/gui/scaleform" || err "clear"
-  rm -rf "$WOT_PATH/res_mods/$TARGET_VERSION/scripts/client/gui/scaleform/locale" || err "clear"
-}
-
-make_dirs()
-{
-  mkdir -p "$WOT_PATH/res_mods/configs/xvm" || err "make_dirs"
-  mkdir -p "$WOT_PATH/res_mods/mods" || err "make_dirs"
-  mkdir -p "$WOT_PATH/res_mods/mods/shared_resources/xvm" || err "make_dirs"
-  mkdir -p "$WOT_PATH/res_mods/$TARGET_VERSION" || err "make_dirs"
-}
-
-copy_xfw()
+install_xfw()
 {
   echo "=> xfw"
-  cp -a ../src/xfw/~output/swf_wg/* ../~output/~ver/gui/flash/
-  cp -a ../src/xfw/~output/swf/* ../~output/mods/xfw/actionscript/
-  cp -a ../src/xfw/~output/python/scripts/* ../~output/~ver/scripts/
-  cp -a ../src/xfw/~output/python/mods/* ../~output/mods/
+  rm -f "$WOT_PATH/mods/$XVMBUILD_WOT_VERSION"/com.modxvm.xfw_*.wotmod || err "[install_xfw]"
+  cp -a ../src/xfw/~output_wotmod/com.modxvm.xfw*.wotmod "$WOT_PATH/mods/$XVMBUILD_WOT_VERSION/" || err "[install_xfw]"
 }
 
-copy_output()
+install_xvm()
 {
-  echo "=> res_mods/$TARGET_VERSION"
-  cp -a ../~output/res_mods/~ver/* "$WOT_PATH/res_mods/$TARGET_VERSION" || err "copy_output"
-
   echo "=> res_mods/mods"
-  cp -a ../~output/res_mods/mods/* "$WOT_PATH/res_mods/mods" || err "copy_output"
+  rm -rf "$WOT_PATH/res_mods/mods/packages" || err "[install_xvm] rm"
+  mkdir -p "$WOT_PATH/res_mods/mods/packages" || err "[install_xvm] mkdir"
+  cp -a ../~output/res_mods/mods/packages/xvm_* "$WOT_PATH/res_mods/mods/packages/" || err "[install_xvm] cp"
 }
 
 copy_configs()
 {
   echo "=> res_mods/configs/xvm"
-  cp -a ../release/configs/* "$WOT_PATH/res_mods/configs/xvm" || err "copy_configs"
+  rm -rf "$WOT_PATH/res_mods/configs/xvm/py_macro/xvm" || err "[copy_xvm_dir] rm"
+  mkdir -p "$WOT_PATH/res_mods/configs/xvm" || err "[copy_configs] mkdir"
+  cp -a ../release/configs/* "$WOT_PATH/res_mods/configs/xvm" || err "[copy_configs] cp configs"
 
   echo "=> res_mods/configs/xvm/xvm.xc"
   if [ -f "test/configs/xvm.xc" ]; then
-    cp -a "test/configs/xvm.xc" "$WOT_PATH/res_mods/configs/xvm/xvm.xc" || err "copy_configs"
+    cp -a "test/configs/xvm.xc" "$WOT_PATH/res_mods/configs/xvm/xvm.xc" || err "[copy_configs] cp xvm.xc"
   else
-    rm -f "$WOT_PATH/res_mods/configs/xvm/xvm.xc" || err "copy_configs"
+    rm -f "$WOT_PATH/res_mods/configs/xvm/xvm.xc" || err "[copy_configs] rm xvm.xc"
   fi
 }
 
@@ -79,8 +60,8 @@ copy_xvm_dir()
   [ -e "$SHARED_RESOURCES_PATH/$1" ] && rm -rf "$SHARED_RESOURCES_PATH/$1"
   [ -d "../release/$1" ] && {
     echo "=> $1"
-    mkdir -p $(dirname "$SHARED_RESOURCES_PATH/$1") || err "copy_xvm_dir"
-    cp -a "../release/$1" "$SHARED_RESOURCES_PATH/$1" || err "copy_xvm_dir"
+    mkdir -p $(dirname "$SHARED_RESOURCES_PATH/$1") || err "[copy_xvm_dir] mkdir"
+    cp -a "../release/$1" "$SHARED_RESOURCES_PATH/$1" || err "[copy_xvm_dir] cp"
   }
 }
 
@@ -89,8 +70,8 @@ copy_xvm_file()
   [ -f "$SHARED_RESOURCES_PATH/$1" ] && rm -f "$SHARED_RESOURCES_PATH/$1"
   [ -f "../release/$1" ] && {
     echo "=> $1"
-    mkdir -p $(dirname "$SHARED_RESOURCES_PATH/$1") || err "copy_xvm_file"
-    cp -a "../release/$1" "$SHARED_RESOURCES_PATH/$1" || err "copy_xvm_file"
+    mkdir -p $(dirname "$SHARED_RESOURCES_PATH/$1") || err "[copy_xvm_file]"
+    cp -a "../release/$1" "$SHARED_RESOURCES_PATH/$1" || err "[copy_xvm_file]"
   }
 }
 
@@ -101,20 +82,17 @@ pushd "$CURRENT_DIR" >/dev/null
 # load config
 . ../build/xvm-build.conf
 
-TARGET_VERSION_PATH=$WOT_PATH/res_mods/$TARGET_VERSION
+XVMBUILD_WOT_VERSION_PATH=$WOT_PATH/res_mods/$XVMBUILD_WOT_VERSION
 SHARED_RESOURCES_PATH=$WOT_PATH/res_mods/mods/shared_resources/xvm
 
 # check config
 check_config
 
 # deploy
-clear
 
-make_dirs
+install_xfw
 
-copy_xfw
-
-copy_output
+install_xvm
 
 copy_configs
 
