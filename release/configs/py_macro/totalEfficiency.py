e5d3ab9cfdf44c39a05b611c06c5e3bcb39dcf53
Александр <avp1114@gmail.com>
2017-04-11 12:09:23 +0300

    totalEfficiency: added the macro {{py:xvm.isStuns}}

--- release/configs/py_macro/totalEfficiency.py
+++ release/configs/py_macro/totalEfficiency.py
@@ -39,6 +39,7 @@ fragsSquad = 0
 fragsSquad_dict = {}
 isPlayerInSquad = False
 totalStun = 0
+isStuns = None
 
 
 ribbonTypes = {
@@ -202,7 +203,7 @@ def onHealthChanged(self, newHealth, attackerID, attackReasonID):
 
 @registerEvent(Vehicle, 'onEnterWorld')
 def onEnterWorld(self, prereqs):
-    global player, isPlayerInSquad
+    global player, isPlayerInSquad, isStuns
     player = BigWorld.player()
     if self.publicInfo['team'] != player.team:
         global vehiclesHealth
@@ -212,13 +213,14 @@ def onEnterWorld(self, prereqs):
         isPlayerInSquad = player.guiSessionProvider.getArenaDP().isSquadMan(player.playerVehicleID)
         vehCD = self.typeDescriptor.type.compactDescr
         maxHealth = self.health
+        isStuns = 'st' if 'stunDuration' in self.typeDescriptor.shot['shell'] else None
 
 
 @registerEvent(PlayerAvatar, '_PlayerAvatar__destroyGUI')
 def destroyGUI(self):
     global vehiclesHealth, totalDamage, totalAssist, totalBlocked, damageReceived, damagesSquad, detection, isPlayerInSquad
     global ribbonTypes, numberHitsBlocked, player, numberHitsDealt, old_totalDamage, damage, numberShotsDealt, totalStun
-    global numberDamagesDealt, numberShotsReceived, numberHitsReceived, numberHits, fragsSquad, fragsSquad_dict
+    global numberDamagesDealt, numberShotsReceived, numberHitsReceived, numberHits, fragsSquad, fragsSquad_dict, isStuns
     vehiclesHealth = {}
     totalDamage = 0
     damage = 0
@@ -240,6 +242,7 @@ def destroyGUI(self):
     fragsSquad_dict = {}
     isPlayerInSquad = False
     totalStun = 0
+    isStuns = None
     ribbonTypes = {
         'armor': 0,
         'damage': 0,
@@ -403,3 +406,8 @@ def xvm_isPlayerInSquad():
 @xvm.export('xvm.dmg', deterministic=False)
 def xvm_dmg():
     return damage
+
+
+@xvm.export('xvm.isStuns', deterministic=False)
+def xvm_isStuns():
+    return isStuns
