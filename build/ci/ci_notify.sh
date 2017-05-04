8462f039e70200af5335f2efa60685657c0cc7f0
Mikhail Paulyshka <mixail@modxvm.com>
2017-05-04 17:51:44 +0300

    build/notify: try to fix error with < and > in commit message

--- build/ci/ci_notify.sh
+++ build/ci/ci_notify.sh
@@ -13,7 +13,7 @@ source "$XVMBUILD_ROOT_PATH/build/library.sh"
 
 htmlencode()
 {
-  local result=$(echo "$1" | sed 's/&/\&amp;/g')
+  local result=$(echo "$1" | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g;')
   echo "$result"
 }
 
