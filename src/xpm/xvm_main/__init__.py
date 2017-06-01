9456ed1c58aafd99aeb736275426b1640268aa55
Mikhail Paulyshka <mixail@modxvm.com>
2017-06-02 01:36:48 +0300

    XVM 6.7.2

--- src/xpm/xvm_main/__init__.py
+++ src/xpm/xvm_main/__init__.py
@@ -5,10 +5,10 @@
 
 XFW_MOD_INFO = {
     # mandatory
-    'VERSION':       '0.9.19.0',
+    'VERSION':       '0.9.19.0.1',
     'URL':           'http://www.modxvm.com/',
     'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
-    'GAME_VERSIONS': ['0.9.19.0'],
+    'GAME_VERSIONS': ['0.9.19.0.1'],
     # optional
 }
 
--- src/xpm/xvm_main/consts.py
+++ src/xpm/xvm_main/consts.py
@@ -1,8 +1,8 @@
 """ XVM (c) www.modxvm.com 2013-2017 """
 
 class XVM(object):
-    XVM_VERSION    = '6.7.2-dev'
-    WOT_VERSION    = '0.9.19.0'
+    XVM_VERSION    = '6.7.2'
+    WOT_VERSION    = '0.9.19.0.1'
     XVM_INTRO      = 'www.modxvm.com'
 
     API_VERSION = '4.0'
