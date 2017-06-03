cb5e1137dd30d728c0c6323a45a4d5fda9e619d0
night_dragon_on <devnull@localhost>
2017-06-03 21:45:06 +0500

    doc: typo

--- src/xpm/xvm_main/vehinfo_short.py
+++ src/xpm/xvm_main/vehinfo_short.py
@@ -639,7 +639,7 @@ _data = {
     "ussr:R98_T44_85":                       't44·85',
     "ussr:R98_T44_85M":                      't44·85m',
     "ussr:R99_T44_122":                      't44·122'
-    # Don't used
+    # Unused
     #"china:Ch04_T34_1_training":             't34·1',
     #"germany:G03_PzV_Panther_training":      'Pant',
     #"germany:G10_PzIII_AusfJ_training":      'Pz3',
