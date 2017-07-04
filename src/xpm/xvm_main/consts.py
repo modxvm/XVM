99b4d6a1b2cfaeed1bb27c491512c18c427f431b
Maxim Schedriviy <m.schedriviy@temabit.com>
2017-07-04 15:44:22 +0300

    bump version in the python modules

--- src/xpm/xvm_main/consts.py
+++ src/xpm/xvm_main/consts.py
@@ -2,7 +2,7 @@
 
 class XVM(object):
     XVM_VERSION    = '6.7.5-dev'
-    WOT_VERSION    = '0.9.19.0.2'
+    WOT_VERSION    = '0.9.19.1'
     XVM_INTRO      = 'www.modxvm.com'
 
     API_VERSION = '4.0'
