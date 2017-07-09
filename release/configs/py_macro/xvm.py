7884217399604ef00fe3750c4525caabde49af9c
Александр <avp1114@gmail.com>
2017-07-09 12:08:54 +0300

    Changes in macroes:{{py:xvm.screenWidth}}, {{py:xvm.screenHeight}}, {{py:xvm.screenVCenter}}, {{py:xvm.screenHCenter}}

--- release/configs/py_macro/xvm.py
+++ release/configs/py_macro/xvm.py
@@ -134,22 +134,22 @@ def total_hp_getMainGun(a, b, dmg_total):
 
 #Screen size
 
-@xvm.export('xvm.screenWidth')
+@xvm.export('xvm.screenWidth', deterministic=False)
 def xvm_screenWidth():
     return BigWorld.screenWidth()
 
 
-@xvm.export('xvm.screenHeight')
+@xvm.export('xvm.screenHeight', deterministic=False)
 def xvm_screenHeight():
     return BigWorld.screenHeight()
 
 
-@xvm.export('xvm.screenVCenter')
+@xvm.export('xvm.screenVCenter', deterministic=False)
 def xvm_screenVCenter():
     return BigWorld.screenHeight() // 2
 
 
-@xvm.export('xvm.screenHCenter')
+@xvm.export('xvm.screenHCenter', deterministic=False)
 def xvm_screenHCenter():
     return BigWorld.screenWidth() // 2
 
