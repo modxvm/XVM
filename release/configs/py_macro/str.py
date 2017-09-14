df5317239644793c3eed6871266b410ca619692e
Александр <avp1114@gmail.com>
2017-09-14 15:51:51 +0300

    added macros {{py:upper('str')}}, {{py:lower('str')}} и {{py:capitalize('str')}}

--- release/configs/py_macro/str.py
+++ release/configs/py_macro/str.py
@@ -1,3 +1,18 @@
 @xvm.export('replace')
 def str_replace(str, old, new, max=-1):
     return str.replace(old, new, max)
+
+
+@xvm.export('upper')
+def str_upper(string):
+    return string.upper()
+
+
+@xvm.export('lower')
+def str_lower(string):
+    return string.lower()
+
+
+@xvm.export('capitalize')
+def str_capitalize(string):
+    return string.capitalize()
