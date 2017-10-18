43b0204cf007e97a8481ad14d28e9162407a510a
Maxim Schedriviy <max@modxvm.com>
2017-10-18 09:39:06 +0300

    add stub for xvm_quests

---
+++ src/xpm/xvm_quests/__init__.py
@@ -0,0 +1,15 @@
+""" XVM (c) www.modxvm.com 2013-2017 """
+
+#####################################################################
+# MOD INFO
+
+XFW_MOD_INFO = {
+    # mandatory
+    'VERSION':       '0.9.20.1',
+    'URL':           'http://www.modxvm.com/',
+    'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
+    'GAME_VERSIONS': ['0.9.20.1'],
+    # optional
+}
+
+# OBSOLETE, WILL BE DELETED
