0dc331cb2bcc4205f2e6f7c6f55133463509f730
Mikhail Paulyshka <me@mixaill.tk>
2018-04-05 09:46:27 +0300

    XVM 7.5.5

--- src/xpm/xvm_main/consts.py
+++ src/xpm/xvm_main/consts.py
@@ -4,7 +4,7 @@ from __version__ import __xvm_version__
 
 class XVM(object):
     XVM_VERSION    = __xvm_version__
-    WOT_VERSION    = '1.0.0.1'
+    WOT_VERSION    = '1.0.0.2'
     XVM_INTRO      = 'https://modxvm.com'
 
     API_VERSION = '4.0'
