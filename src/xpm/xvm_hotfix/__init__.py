63264dddd96bee1c320836656ab265e1e36659ea
sirmax2 <max@modxvm.com>
2018-10-23 23:58:43 +0300

    fix filecache

--- src/xpm/xvm_hotfix/__init__.py
+++ src/xpm/xvm_hotfix/__init__.py
@@ -50,11 +50,18 @@ def _MarkersManager_createMarker(base, self, *args, **kwargs):
 
 ######################
 
+import base64
 from account_helpers.CustomFilesCache import CustomFilesCache
 @overrideMethod(CustomFilesCache, '_CustomFilesCache__onReadLocalFile')
 def _CustomFilesCache__onReadLocalFile(base, self, url, showImmediately):
     try:
         base(self, url, showImmediately)
-    except Exception:
-        err('CustomFilesCache.__onReadLocalFile: url="{0}", showImmediately={1}'.format(url, showImmediately))
+    except EOFError:
+        err('CustomFilesCache.__onReadLocalFile: url="{0}"'.format(url))
         err(traceback.format_exc())
+        try:
+            log('Attempt to reload url: {0}'.format(url))
+            del(self._CustomFilesCache__db[base64.b32encode(url)])
+            base(self, url, showImmediately)
+        except Exception:
+            err(traceback.format_exc())
