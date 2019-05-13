8d41f8f19849a418ddaec2596c6223b37362d5d0
Mikhail Paulyshka <me@mixaill.tk>
2019-05-13 14:49:01 +0300

    build/ci: send to TG

--- build/ci/ci_notify.sh
+++ build/ci/ci_notify.sh
@@ -32,7 +32,7 @@ check_variables()
 {
   if [ "$XVMBUILD_URL_DOWNLOAD" == "" ]; then
     echo "No download URL"
-    eturn 1
+    return 1
   fi
 
   if [ "$XVMBUILD_IPB_APIKEY" == "" ]; then
@@ -73,6 +73,20 @@ post_ipb()
   curl -k -sS -m 2 -H "Content-Type: application/x-www-form-urlencoded" -H "User-Agent: XVM Build Server\r\n" -X POST --data "$XVMBUILD_IPB_REQBODY" "$XVMBUILD_IPB_REQURL" --output /dev/null || true
 }
 
+post_telegram()
+{
+  downloadlinkzip="$XVMBUILD_URL_DOWNLOAD"/"$XVMBUILD_XVM_BRANCH"/"$XVMBUILD_XVM_REVISION"_"$XVMBUILD_XVM_HASH"_xvm.zip
+  downloadlinkexe="$XVMBUILD_URL_DOWNLOAD"/"$XVMBUILD_XVM_BRANCH"/"$XVMBUILD_XVM_REVISION"_"$XVMBUILD_XVM_BRANCH"_xvm.exe
+  builddate=$(date --utc +"%d.%m.%Y %H:%M (UTC)")
+
+  XVMBUILD_XVM_COMMITAUTHOR=$(htmlencode "$XVMBUILD_XVM_COMMITAUTHOR")
+  XVMBUILD_XVM_COMMITMSG=$(htmlencode "$XVMBUILD_XVM_COMMITMSG")
+  
+  printf "<b>Build: </b><a href='$XVMBUILD_URL_REPO/$XVMBUILD_XVM_HASH'>$XVMBUILD_XVM_REVISION (branch $XVMBUILD_XVM_BRANCH)</a>\n<b>Date:</b> $builddate\n<b>Download: </b><a href='$downloadlinkzip'>.zip archive</a> | <a href='$downloadlinkexe'>.exe installer</a>\n<b>Author:</b>$XVMBUILD_XVM_COMMITAUTHOR\n<b>Description:</b> $XVMBUILD_XVM_COMMITMSG" | telegram-send --stdin --format html --config /var/xvm/telegram-send.conf
+}
+
 load_repositorystats
 check_variables
+
 post_ipb
+post_telegram
