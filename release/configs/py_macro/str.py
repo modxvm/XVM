3d63d1e3dd6f80cca769a9c4778bfe58e2283ab2
ktulho <avp1114@gmail.com>
2019-05-14 13:09:02 +0300

    added macros {{py:strip(string, chars)}}, {{py:lstrip(string, chars)}}, {{py:rstrip(string, chars)}}

--- release/configs/py_macro/str.py
+++ release/configs/py_macro/str.py
@@ -5,19 +5,34 @@ def str_replace(str, old, new, max=-1):
 
 @xvm.export('upper')
 def str_upper(string):
-    return unicode(string, 'utf-8').upper()
+    return unicode(string, 'utf-8').upper() if isinstance(string, basestring) else string
 
 
 @xvm.export('lower')
 def str_lower(string):
-    return unicode(string, 'utf-8').lower()
+    return unicode(string, 'utf-8').lower() if isinstance(string, basestring) else string
 
 
 @xvm.export('capitalize')
 def str_capitalize(string):
-    return unicode(string, 'utf-8').capitalize()
+    return unicode(string, 'utf-8').capitalize() if isinstance(string, basestring) else string
 
 
 @xvm.export('title')
 def str_title(string):
-    return unicode(string, 'utf-8').title()
+    return unicode(string, 'utf-8').title() if isinstance(string, basestring) else string
+
+
+@xvm.export('strip')
+def str_strip(string, chars=' '):
+    return unicode(string, 'utf-8').strip(chars) if isinstance(string, basestring) else string
+
+
+@xvm.export('lstrip')
+def str_lstrip(string, chars=' '):
+    return unicode(string, 'utf-8').lstrip(chars) if isinstance(string, basestring) else string
+
+
+@xvm.export('rstrip')
+def str_rstrip(string, chars=' '):
+    return unicode(string, 'utf-8').rstrip(chars) if isinstance(string, basestring) else string
