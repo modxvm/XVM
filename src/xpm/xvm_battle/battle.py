af098e6ce6b5af7c8a75d33f6f4777bb3cd38b90
AstaRom <astafev.roman.mail@gmail.com>
2019-08-07 14:25:40 +0500

    add: some options

--- src/xpm/xvm_battle/battle.py
+++ src/xpm/xvm_battle/battle.py
@@ -234,6 +234,13 @@ def canDisplayHelpHint(base, self, typeDescriptor):
         return False
     base(self, typeDescriptor)
 
+@overrideMethod(SharedPage, '_definePostmortemPanel')
+def _definePostmortemPanel(base, self):
+    if not config.get('battle/showPostmortemDogtag'):
+        self.as_useEventPostmortemPanelS(False)
+        return
+    base(self)
+
 
 #####################################################################
 # Battle
