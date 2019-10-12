2d89ac142fba75ad49e35cbe680306695c08b3f7
Mikhail Paulyshka <me@mixaill.tk>
2019-10-12 04:25:14 +0300

    xfw_ping: add support for new loader

--- src/xfw_packages/xfw_ping/python/__init__.py
+++ src/xfw_packages/xfw_ping/python/__init__.py
@@ -16,64 +16,76 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import imp
-import os
-import traceback
-import sys
+#cpython
+import logging
 
-from xfw.constants import PATH
+#xfw.loader
+import xfw_loader.python as loader
 
-_ping_native = None
-
-def _native_load():
-    """
-    Loads Ping extension.
-
-    Do not use this function directly.
-    """
-    try:
-        global _ping_native
-
-        if _ping_native is None:
-
-            if "python27" not in sys.modules:
-                return False
-
-            path_realfs = PATH.XFWLOADER_PACKAGES_REALFS + '/xfw_ping/native/xfw_ping.pyd'
-            path_vfs = PATH.XFWLOADER_PACKAGES_VFS + '/xfw_ping/native/xfw_ping.pyd'
-
-            if os.path.isfile(path_realfs):
-                _ping_native = imp.load_dynamic('XFW_Ping', path_realfs)
-            else:
-                import xfw.vfs as vfs
-                _ping_native = vfs.c_extension_load('XFW_Ping', path_vfs, 'com.modxvm.xfw.ping')
-
-        return True
-    except Exception:
-        print "[XFW/Ping][_native_load] Error when load native library"
-        traceback.print_exc()
-        print "======================="
-        return False
+g_xfwping = None
 
 def ping(address, unlock_gil=True):
-    """
-    Send ping request to host
+    if not g_xfwping:
+        return None
+    return g_xfwping.ping(address, unlock_gil)
 
-    usage: ping(<address>, [unlock_gil])
-      address: host domain name or IP
-      unlock_gil: unlock GIL when perform I/O operations, True/False, True by default.
+class XFWPing(object):
 
-    returns: ping time in msec
+    def __init__(self):
+        self.__native = None
+        self.__package_name = 'com.modxvm.xfw.ping'
+        self.__initialized = False
 
-    examples:
-      ping("127.0.0.1")
-      ping("localhost",True)
-    """
-    try:
-        if not _native_load():
-            return None
-        return _ping_native.ping(address, unlock_gil)
-    except Exception:
-        print "[XFW/Ping][ping] Error when pinging host"
-        traceback.print_exc()
-        print "======================="
+        try:
+            xfwnative = loader.get_mod_module('com.modxvm.xfw.native')
+            if not xfwnative:
+                logging.error('[XFW/Ping] Failed to load native module. XFW Native is not available')
+                return
+
+            if not xfwnative.unpack_native(self.__package_name):
+                logging.error('[XFW/Ping] Failed to load native module. Failed to unpack native module')
+                return
+
+            self.__native = xfwnative.load_native(self.__package_name, 'xfw_ping.pyd', 'XFW_Ping')
+            if not self.__native:
+                logging.error("[XFW/Ping] Failed to load native module. Crash report were not enabled")   
+                return
+
+            self.__initialized = True
+        except Exception:
+            logging.exception("[XFW/Ping] Error when loading native library:")
+
+
+    def is_initialized(self):
+        return self.__initialized
+
+
+    def ping(self, address, unlock_gil=True):
+        """
+        Send ping request to host
+
+        usage: ping(<address>, [unlock_gil])
+        address: host domain name or IP
+        unlock_gil: unlock GIL when perform I/O operations, True/False, True by default.
+
+        returns: ping time in msec
+
+        examples:
+        ping("127.0.0.1")
+        ping("localhost",True)
+        """
+        try:
+            if not self.__native:
+                return None
+            return self.__native.ping(address, unlock_gil)
+        except Exception:
+            logging.exception("[XFW/Ping][ping]")
+
+
+def xfw_is_module_loaded():
+    if not g_xfwping:
+        return False
+
+    return g_xfwping.is_initialized()
+
+g_xfwping = XFWPing()
