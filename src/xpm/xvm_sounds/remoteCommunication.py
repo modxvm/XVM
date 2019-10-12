5933ffbf523deaed89bd7605a8d4f97232d48fc0
Mikhail Paulyshka <me@mixaill.tk>
2019-10-12 19:42:20 +0300

    xvm_sounds: get xfw_wwise module using xfw_loader

--- src/xpm/xvm_sounds/remoteCommunication.py
+++ src/xpm/xvm_sounds/remoteCommunication.py
@@ -1,10 +1,43 @@
-""" XVM (c) https://modxvm.com 2013-2019 """
+"""
+This file is part of the XVM project.
 
-from xfw_wwise.python import g_wwise as wwise
+Copyright (c) 2013-2019 XVM Team.
+
+XVM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, version 3.
+
+XVM is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+#cpython
+import logging
+
+#xfw.loader
+import xfw_loader.python as loader
+
+#xvm.main
 import xvm_main.python.config as config
 
-if config.get('sounds/remote_communication', False):
-    try:
-        wwise.comm_init()
-    except Exception:
-        print "xvm_sounds/remotecommunication: failed to start remote communication"
+if loader.is_mod_loaded('com.modxvm.xfw.wwise'):
+    if config.get('sounds/remote_communication', False):
+        try:
+            wwise_module = loader.get_mod_module('com.modxvm.xfw.wwise')
+            if not wwise_module:
+                logging.error("[XVM/Sounds] [remote_communication] XFW.WWISE is failed to load")
+
+            wwise = getattr(wwise_module, 'g_wwise')
+            wwise.comm_init()
+            logging.info("[XVM/Sounds] [remote_communication] communication with WWISE Authoring Tools is enabled")
+
+        except Exception:
+            logging.exception("[XVM/Sounds] [remote_communication]:")
+
+else:
+    logging.warning('[XVM/Sounds] [remote_communication] failed to load sound banks because XFW.WWISE is not loaded')
