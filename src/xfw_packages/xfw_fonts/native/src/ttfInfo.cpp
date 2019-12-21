c94344a3b866fd82b06b2ec360639d9c2aac171d
Mikhail Paulyshka <me@mixaill.tk>
2019-12-21 22:53:16 +0300

    xfw/xfw_fonts: fix loading fonts with unicode encoding

--- src/xfw_packages/xfw_fonts/native/src/ttfInfo.cpp
+++ src/xfw_packages/xfw_fonts/native/src/ttfInfo.cpp
@@ -44,13 +44,70 @@ typedef struct _tagTT_NAME_RECORD{
 
 
 
+std::string ws2s(const std::wstring& s)
+{
+    int slength = (int)s.length() + 1;
+    int len = WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, 0, 0, 0, 0);
+    std::string r(len, '\0');
+    WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, &r[0], len, 0, 0);
+    return r;
+}
+
+std::string getNameRecord(std::ifstream& fileStream, TT_TABLE_DIRECTORY tblDir, int id) {
+    fileStream.seekg(tblDir.uOffset);
+
+    TT_NAME_TABLE_HEADER ttNTHeader{};
+    fileStream.read(reinterpret_cast<char*>(&ttNTHeader), sizeof(ttNTHeader));
+
+    ttNTHeader.uNRCount = SWAPWORD(ttNTHeader.uNRCount);
+    ttNTHeader.uStorageOffset = SWAPWORD(ttNTHeader.uStorageOffset);
+
+    TT_NAME_RECORD ttRecord{};
+    for (uint16_t i = 0; i < ttNTHeader.uNRCount; i++)
+    {
+        fileStream.read(reinterpret_cast<char*>(&ttRecord), sizeof(ttRecord));
+        ttRecord.uNameID = SWAPWORD(ttRecord.uNameID);
+        if (ttRecord.uNameID != id)
+            continue;
+
+        ttRecord.uStringLength = SWAPWORD(ttRecord.uStringLength);
+        ttRecord.uStringOffset = SWAPWORD(ttRecord.uStringOffset);
+        const auto n_pos = fileStream.tellg();
+
+        fileStream.seekg(tblDir.uOffset + ttRecord.uStringOffset + ttNTHeader.uStorageOffset);
+
+
+        if (SWAPWORD(ttRecord.uEncodingID) == 1) {
+            const auto buf = static_cast<wchar_t*>(calloc(ttRecord.uStringLength / 2 + 1, sizeof(wchar_t)));
+            fileStream.read(reinterpret_cast<char*>(buf), ttRecord.uStringLength);
+            for (auto i = 0; i < ttRecord.uStringLength / 2; i++) {
+                buf[i] = SWAPWORD(buf[i]);
+            }
+            if (wcslen(reinterpret_cast<wchar_t*>(buf)) > 0) {
+                return ws2s(std::wstring(reinterpret_cast<wchar_t*>(buf)));
+            }
+        }
+        else {
+            const auto buf = static_cast<char*>(calloc(ttRecord.uStringLength + 1, sizeof(char)));
+            fileStream.read(buf, ttRecord.uStringLength);
+            if (strlen(buf) > 0) {
+                return std::string(buf);
+            }
+        }
+
+        fileStream.seekg(n_pos);
+    }
+
+    return "";
+}
+
 std::string GetFontFamilyFromFile(std::wstring& filePath)
 {
     std::ifstream fileStream;
 
     fileStream.open(filePath, std::ifstream::binary);
 
-    if(!fileStream.is_open())
+    if (!fileStream.is_open())
     {
         return "";
     }
@@ -62,7 +119,7 @@ std::string GetFontFamilyFromFile(std::wstring& filePath)
     ttOffsetTable.uMinorVersion = SWAPWORD(ttOffsetTable.uMinorVersion);
 
     //check is this is a true type font and the version is 1.0
-    if(ttOffsetTable.uMajorVersion != 1 || ttOffsetTable.uMinorVersion != 0)
+    if (ttOffsetTable.uMajorVersion != 1 || ttOffsetTable.uMinorVersion != 0)
     {
         fileStream.close();
         return "";
@@ -71,13 +128,10 @@ std::string GetFontFamilyFromFile(std::wstring& filePath)
     bool bFound = false;
 
     TT_TABLE_DIRECTORY tblDir{};
-    for(uint16_t i=0; i< ttOffsetTable.uNumOfTables; i++)
+    for (uint16_t i = 0; i < ttOffsetTable.uNumOfTables; i++)
     {
-        ZeroMemory(&tblDir, sizeof(tblDir));
-
-        fileStream.seekg(sizeof(TT_OFFSET_TABLE) + sizeof(TT_TABLE_DIRECTORY)*i);
         fileStream.read(reinterpret_cast<char*>(&tblDir), sizeof(TT_TABLE_DIRECTORY));
-        if(strncmp(tblDir.szTag,"name",4) == 0)
+        if (strncmp(tblDir.szTag, "name", 4) == 0)
         {
             bFound = TRUE;
             tblDir.uLength = SWAPLONG(tblDir.uLength);
@@ -86,47 +140,13 @@ std::string GetFontFamilyFromFile(std::wstring& filePath)
         }
     }
 
-    if(!bFound)
+    if (!bFound)
     {
         fileStream.close();
         return "";
     }
 
-    fileStream.seekg(tblDir.uOffset);
-
-    TT_NAME_TABLE_HEADER ttNTHeader{};
-    fileStream.read(reinterpret_cast<char*>(&ttNTHeader), sizeof(ttNTHeader));
-    ttNTHeader.uNRCount = SWAPWORD(ttNTHeader.uNRCount);
-    ttNTHeader.uStorageOffset = SWAPWORD(ttNTHeader.uStorageOffset);
-
-    TT_NAME_RECORD ttRecord{};
-    bFound = false;
-
-    for(uint16_t i=0; i<ttNTHeader.uNRCount; i++)
-    {
-        fileStream.read(reinterpret_cast<char*>(&ttRecord), sizeof(ttRecord));
-        ttRecord.uNameID = SWAPWORD(ttRecord.uNameID);
-        if(ttRecord.uNameID != 1)
-            continue;
-
-        ttRecord.uStringLength = SWAPWORD(ttRecord.uStringLength);
-        ttRecord.uStringOffset = SWAPWORD(ttRecord.uStringOffset);
-        const auto n_pos = fileStream.tellg();
-
-        fileStream.seekg(tblDir.uOffset + ttRecord.uStringOffset + ttNTHeader.uStorageOffset);
-
-
-        const auto buf = static_cast<char*>(calloc(ttRecord.uStringLength + 1, sizeof(char)));
-        fileStream.read(buf, ttRecord.uStringLength);
-        if(strlen(buf)>0)
-        {
-            fileStream.close();
-            return std::string(buf);
-        }
-
-        fileStream.seekg(n_pos);
-    }
-
+    auto id_name = getNameRecord(fileStream, tblDir, 1);
     fileStream.close();
-    return "";
+    return id_name;
 }
