modxvm/XVM

commit bf6eed12e47ba8d97f65f1cc2a1bd5c931f6ee50

fix badgeId

src/xpm/xvm_main/stats.py
@@ -688,8 +688,7 @@ class _Player(object):
         self.clan = vData['clanAbbrev']
         self.badgeId = None
         ranked = vData.get('ranked', None)
-        if ranked and ranked[1]:
-            self.badgeId = str(ranked[1][0])
+        self.badgeId = str(ranked[1])
         self.clanInfo = topclans.getClanInfo(self.clan)
         self.vehCD = None
         if 'typeCompDescr' in vData:

commit 5c88114a286bc10c55099140ce67a1707f845a9d

xpm/xvm_main: fix regression in _Player::__init__() introduced in bf6eed12

src/xpm/xvm_main/stats.py
@@ -688,7 +688,8 @@ class _Player(object):
         self.clan = vData['clanAbbrev']
         self.badgeId = None
         ranked = vData.get('ranked', None)
-        self.badgeId = str(ranked[1])
+        if ranked:
+            self.badgeId = str(ranked[1])
         self.clanInfo = topclans.getClanInfo(self.clan)
         self.vehCD = None
         if 'typeCompDescr' in vData: