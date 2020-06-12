bf62071d8ab6855ba77b87ceb781d06dc98c6b8d
AstaRom <astafev.roman.mail@gmail.com>
2020-06-12 11:47:54 +0500

    upd: vehicleNames

--- src/xpm/xvm_main/vehinfo_tiers.py
+++ src/xpm/xvm_main/vehinfo_tiers.py
@@ -15,7 +15,7 @@ from skeletons.gui.shared import IItemsCache
 
 _special = {
     # Data from http://forum.worldoftanks.ru/index.php?/topic/1894923-
-    # Last update: 06.06.2020
+    # Last update: 12.06.2020
 
     # level 2
     'germany:G53_PzI':                     [ 2, 2 ],
