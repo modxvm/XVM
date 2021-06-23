14e8582c44cb67c2c91dafadcf894b9da3dc6c22
Mikhail Paulyshka <me@mixaill.tk>
2021-06-23 23:01:08 +0300

    xpm/export: add stub files
    to prevent loading old modules for users which are updating from old version

---
+++ src/xpm/xvm_export/__init__.py
@@ -0,0 +1,31 @@
+"""
+This file is part of the XVM project.
+
+Copyright (c) 2013-2021 XVM Team.
+
+XVM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, version 3.
+
+XVM is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+#
+# XFW Mod initialization
+#
+
+__xvm_main_loaded = False
+
+def xfw_module_init():
+    global __xvm_main_loaded
+    __xvm_main_loaded = True
+
+
+def xfw_is_module_loaded():
+    return __xvm_main_loaded
