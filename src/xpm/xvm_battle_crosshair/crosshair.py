c8a554295582e4d854f0674f66c2a7c59d968487
Mikhail Paulyshka <me@mixaill.tk>
2023-03-07 19:43:39 +0300

    packages: add net.openwg.fix.crosshairs v1.0.1
    Co-authored-by: Andrii Andrushchyshyn <poliroid@pm.me>

--- src/xpm/xvm_battle_crosshair/crosshair.py
+++ src/xpm/xvm_battle_crosshair/crosshair.py
@@ -7,29 +7,6 @@ Copyright (c) 2016-2023 XVM Contributors
 # imports
 #
 
-# BigWorld
-import BigWorld
-from gui.Scaleform.daapi.view.battle.shared.crosshair.plugins import TargetDistancePlugin
-
-# XFW
-from xfw.events import overrideMethod
-
-
-#
-# Avatar
-#
-
-def _TargetDistancePlugin__startTrack(base, self, vehicleIDs):
-    try:
-        self._TargetDistancePlugin__stopTrack(immediate=True)
-        target = BigWorld.entity(vehicleID)
-        if target is not None:
-            self._TargetDistancePlugin__trackID = vehicleID
-            self._TargetDistancePlugin__updateDistance(target)
-            self._interval.start()
-    except Exception:
-        logging.getLogger('XVM/Battle/Crosshair').exception('_TargetDistancePlugin__startTrack')
-
 
 
 #
@@ -37,9 +14,7 @@ def _TargetDistancePlugin__startTrack(base, self, vehicleIDs):
 #
 
 def init():
-    # TargetDistancePlugin
-    overrideMethod(TargetDistancePlugin, '_TargetDistancePlugin__startTrack')(_TargetDistancePlugin__startTrack)
-
+    pass
 
 def fini():
     pass
