31f0b64d165e8fe6570e2495a7d51a18b2cd653d
Mikhail Paulyshka <me@mixaill.net>
2023-10-15 21:54:03 +0300

    python/limits: fix exceptions for WoT 1.22.1

--- src/python/xvm_limits/__init__.py
+++ src/python/xvm_limits/__init__.py
@@ -1,19 +1,16 @@
-﻿""" XVM (c) https://modxvm.com 2013-2021 """
+﻿"""
+SPDX-License-Identifier: GPL-3.0-or-later
+Copyright (c) 2013-2023 XVM Contributors
+"""
 
-#####################################################################
-# constants
+#
+# Imports
+#
 
-class XVM_LIMITS_COMMAND(object):
-    SET_GOLD_LOCK_STATUS = "xvm_limits.set_gold_lock_status"
-    SET_FREEXP_LOCK_STATUS = "xvm_limits.set_freexp_lock_status"
-    SET_CRYSTAL_LOCK_STATUS = "xvm_limits.set_crystal_lock_status"
+# stdlib
+import logging
 
-
-#####################################################################
-# imports
-
-import traceback
-
+# BigWorld
 import BigWorld
 import game
 from gui import shop
@@ -25,9 +22,6 @@ from gui.shared.utils.requesters.StatsRequester import StatsRequester
 from gui.Scaleform.daapi.view.lobby.techtree.settings import UNKNOWN_VEHICLE_LEVEL
 from gui.Scaleform.daapi.view.lobby.techtree.techtree_page import TechTree
 from gui.Scaleform.daapi.view.lobby.techtree.research_page import Research
-from gui.Scaleform.daapi.view.lobby.recruitWindow.RecruitWindow import RecruitWindow
-from gui.Scaleform.daapi.view.lobby.PersonalCase import PersonalCase
-from gui.Scaleform.daapi.view.lobby.exchange.ExchangeFreeToTankmanXpWindow import ExchangeFreeToTankmanXpWindow
 from gui.Scaleform.daapi.view.lobby.customization.main_view import MainView
 from gui.Scaleform.genConsts.CURRENCIES_CONSTANTS import CURRENCIES_CONSTANTS
 from gui.shared.formatters import text_styles
@@ -35,40 +29,36 @@ from gui.shared.money import Currency
 from gui.shared.tooltips.common import HeaderMoneyAndXpTooltipData
 from helpers import dependency
 from skeletons.gui.shared import IItemsCache
+from gui.impl.lobby.crew.dialogs.enlarge_barracks_dialog import EnlargeBarracksDialog
 
+# XFW
 from xfw import *
 
+# XVM
 from xvm_main.python.consts import XVM_EVENT
 from xvm_main.python.logger import *
 import xvm_main.python.config as config
 
 
-#####################################################################
-# globals
+
+#
+# Constants
+#
+
+class XVM_LIMITS_COMMAND(object):
+    SET_GOLD_LOCK_STATUS = "xvm_limits.set_gold_lock_status"
+    SET_FREEXP_LOCK_STATUS = "xvm_limits.set_freexp_lock_status"
+    SET_CRYSTAL_LOCK_STATUS = "xvm_limits.set_crystal_lock_status"
+
+
+
+#
+# Configuration
+#
 
 cfg_hangar_enableGoldLocker = False
 cfg_hangar_enableFreeXpLocker = False
 cfg_hangar_enableCrystalLocker = False
-
-gold_enable = True
-freeXP_enable = True
-crystal_enable = True
-TechTree_handler = None
-Research_handler = None
-TechnicalMaintenance_handler = None
-RecruitWindow_handler = None
-PersonalCase_handlers = []
-ExchangeFreeToTankmanXpWindow_handlers = []
-MainView_handler = None
-
-
-#####################################################################
-# initialization/finalization
-
-def start():
-    g_eventBus.addListener(XFW_COMMAND.XFW_CMD, onXfwCommand)
-
-BigWorld.callback(0, start)
 
 def onConfigLoaded(self, e=None):
     global cfg_hangar_enableGoldLocker
@@ -78,26 +68,39 @@ def onConfigLoaded(self, e=None):
     cfg_hangar_enableFreeXpLocker = config.get('hangar/enableFreeXpLocker', False) == True
     cfg_hangar_enableCrystalLocker = config.get('hangar/enableCrystalLocker', False) == True
 
-g_eventBus.addListener(XVM_EVENT.CONFIG_LOADED, onConfigLoaded)
-
-@registerEvent(game, 'fini')
-def fini():
-    g_eventBus.removeListener(XFW_COMMAND.XFW_CMD, onXfwCommand)
-    g_eventBus.removeListener(XVM_EVENT.CONFIG_LOADED, onConfigLoaded)
 
 
-#####################################################################
-# onXfwCommand
+#
+# Handlers/XFW
+#
 
-# returns: (result, status)
+gold_enable = True
+freeXP_enable = True
+crystal_enable = True
+
+
+# run function that updates gold/freeXP/crystal status in active handlers
+def handlersInvalidate(function, *handlers):
+    try:
+        handlers_arr = []
+        for element in handlers:
+            if type(element) in [list, tuple]:
+                handlers_arr.extend(element)
+            else:
+                handlers_arr.append(element)
+        for handler in handlers_arr:
+            if handler: # is active
+                eval('handler.%s' % function)
+    except Exception:
+        logging.exception('xfw_limits/handlersInvalidate')
+
+
 def onXfwCommand(cmd, *args):
     try:
         if cmd == XVM_LIMITS_COMMAND.SET_GOLD_LOCK_STATUS:
             global gold_enable
             gold_enable = not args[0]
             handlersInvalidate('invalidateGold()', TechTree_handler, Research_handler)
-            handlersInvalidate('onGoldChange(0)', TechnicalMaintenance_handler)
-            handlersInvalidate('onGoldChange(0)', RecruitWindow_handler)
             handlersInvalidate("_PersonalCase__onClientChanged({'stats': 'gold'})", PersonalCase_handlers)
             handlersInvalidate("onBuyConfirmed(False)", MainView_handler)
             return (None, True)
@@ -113,66 +116,60 @@ def onXfwCommand(cmd, *args):
             crystal_enable = not args[0]
             handlersInvalidate("_PersonalCase__onClientChanged({'stats': 'crystal'})", PersonalCase_handlers)
             return (None, True)
-    except Exception, ex:
-        err(traceback.format_exc())
+    except Exception:
+        logging.exception('xfw_limits/onXfwCommand')
         return (None, True)
     return (None, False)
 
-# run function that updates gold/freeXP/crystal status in active handlers
-def handlersInvalidate(function, *handlers):
-    try:
-        handlers_arr = []
-        for element in handlers:
-            if type(element) in [list, tuple]:
-                handlers_arr.extend(element)
-            else:
-                handlers_arr.append(element)
-        for handler in handlers_arr:
-            if handler: # is active
-                eval('handler.%s' % function)
-    except Exception, ex:
-        err(traceback.format_exc())
 
 
-#####################################################################
-# handlers
+
+#
+# Handlers/Shop
+#
 
 # enable or disable active usage of gold
-@overrideMethod(shop, 'canBuyGoldForItemThroughWeb')
-def canBuyGoldForItemThroughWeb(base, itemID, *args, **kwargs):
+def shop_canBuyGoldForItemThroughWeb(base, itemID, *args, **kwargs):
     if not cfg_hangar_enableGoldLocker or gold_enable:
         return base(itemID, *args, **kwargs)
     return False
 
-@overrideMethod(shop, 'canBuyGoldForVehicleThroughWeb')
-def canBuyGoldForVehicleThroughWeb(base, vehicle, *args, **kwargs):
+
+def shop_canBuyGoldForVehicleThroughWeb(base, vehicle, *args, **kwargs):
     if not cfg_hangar_enableGoldLocker or gold_enable:
         return base(vehicle, *args, **kwargs)
     return False
 
+
+
+#
+# Handlers/StatsRequester
+#
+
 # enable or disable usage of gold
-@overrideMethod(StatsRequester, 'gold')
 def StatsRequester_gold(base, self):
     if not cfg_hangar_enableGoldLocker or gold_enable:
         return max(self.actualGold, 0)
     return 0
 
 # enable or disable usage of free experience
-@overrideMethod(StatsRequester, 'freeXP')
 def StatsRequester_freeXP(base, self):
     if not cfg_hangar_enableFreeXpLocker or freeXP_enable:
         return max(self.actualFreeXP, 0)
     return 0
 
 # enable or disable usage of bond
-@overrideMethod(StatsRequester, 'crystal')
 def StatsRequester_crystal(base, self):
     if not cfg_hangar_enableCrystalLocker or crystal_enable:
         return max(self.actualCrystal, 0)
     return 0
 
-@overrideMethod(HeaderMoneyAndXpTooltipData, '_getValue')
-def _getValue(base, self):
+
+#
+# Handlers/HeaderMoneyAndXpTooltipData
+#
+
+def HeaderMoneyAndXpTooltipData_getValue(base, self):
     valueStr = '0'
     if self._btnType == CURRENCIES_CONSTANTS.GOLD:
         valueStr = text_styles.gold(backport.getIntegralFormat(max(self.itemsCache.items.stats.actualGold, 0)))
@@ -186,106 +183,150 @@ def _getValue(base, self):
         valueStr = text_styles.expText(backport.getIntegralFormat(max(self.itemsCache.items.stats.actualFreeXP, 0)))
     return valueStr
 
-@overrideMethod(MoneyBalance, '_MoneyBalance__setStats')
+
+
+#
+# Handlers/MoneyBalance
+#
+
 def MoneyBalance__setStats(base, self, model):
     base(self, model)
     model.setCredits(int(self._stats.actualMoney.getSignValue(Currency.CREDITS)))
     model.setGold(int(self._stats.actualMoney.getSignValue(Currency.GOLD)))
     model.setCrystals(int(self._stats.actualMoney.getSignValue(Currency.CRYSTAL)))
 
-@overrideMethod(FullScreenDialogView, '_FullScreenDialogView__setStats')
+
+
+#
+# Handlers/FullScreenDialogView
+#
+
 def FullScreenDialogView__setStats(base, self, model):
     base(self, model)
     model.setCredits(int(self._stats.actualMoney.getSignValue(Currency.CREDITS)))
     model.setGolds(int(self._stats.actualMoney.getSignValue(Currency.GOLD)))
     model.setCrystals(int(self._stats.actualMoney.getSignValue(Currency.CRYSTAL)))
 
-##############################################################
-# handlers of windows that use gold/freeXP/crystal
 
-@registerEvent(TechTree, '_populate')
-def TechTree_populate(self, *args, **kwargs):
+
+#
+# Handlers/TechTree
+#
+
+TechTree_handler = None
+
+def TechTree_populate(base, self, *args, **kwargs):
     global TechTree_handler
     TechTree_handler = self
+    return base(self, *args, **kwargs)
 
-@registerEvent(TechTree, '_dispose')
-def TechTree_dispose(self, *args, **kwargs):
+def TechTree_dispose(base, self, *args, **kwargs):
     global TechTree_handler
     TechTree_handler = None
+    return base(self, *args, **kwargs)
 
-@registerEvent(Research, '_populate')
-def Research_populate(self, *args, **kwargs):
+
+
+#
+# Handlers/Research
+#
+
+Research_handler = None
+
+def Research_populate(base, self, *args, **kwargs):
     global Research_handler
     Research_handler = self
+    return base(self, *args, **kwargs)
 
-@registerEvent(Research, '_dispose')
-def Research_dispose(self, *args, **kwargs):
+def Research_dispose(base, self, *args, **kwargs):
     global Research_handler
     Research_handler = None
+    return base(self, *args, **kwargs)
 
-@registerEvent(RecruitWindow, '_populate')
-def RecruitWindow_populate(self, *args, **kwargs):
-    global RecruitWindow_handler
-    RecruitWindow_handler = self
 
-@registerEvent(RecruitWindow, '_dispose')
-def RecruitWindow_dispose(self, *args, **kwargs):
-    global RecruitWindow_handler
-    RecruitWindow_handler = None
 
-@registerEvent(PersonalCase, '_populate')
-def PersonalCase_populate(self, *args, **kwargs):
-    global PersonalCase_handlers
-    PersonalCase_handlers.append(self)
+#
+# Handlers/MainView
+#
 
-@registerEvent(PersonalCase, '_dispose')
-def PersonalCase_dispose(self, *args, **kwargs):
-    global PersonalCase_handlers
-    if self in PersonalCase_handlers:
-        PersonalCase_handlers.remove(self)
-    else:
-        err('PersonalCase window is disposed without being populated')
+MainView_handler = None
 
-@registerEvent(ExchangeFreeToTankmanXpWindow, '_populate')
-def ExchangeFreeToTankmanXpWindow_populate(self, *args, **kwargs):
-    global ExchangeFreeToTankmanXpWindow_handlers
-    ExchangeFreeToTankmanXpWindow_handlers.append(self)
-
-@registerEvent(ExchangeFreeToTankmanXpWindow, '_dispose')
-def ExchangeFreeToTankmanXpWindow_dispose(self, *args, **kwargs):
-    global ExchangeFreeToTankmanXpWindow_handlers
-    if self in ExchangeFreeToTankmanXpWindow_handlers:
-        ExchangeFreeToTankmanXpWindow_handlers.remove(self)
-    else:
-        err('ExchangeFreeToTankmanXpWindow window is disposed without being populated')
-
-@registerEvent(MainView, '_populate')
-def MainView_populate(self, *args, **kwargs):
+def MainView_populate(base, self, *args, **kwargs):
     global MainView_handler
     MainView_handler = self
+    return base(self, *args, **kwargs)
 
-@registerEvent(MainView, '_dispose')
-def MainView_dispose(self, *args, **kwargs):
+def MainView_dispose(base, self, *args, **kwargs):
     global MainView_handler
     MainView_handler = None
+    return base(self, *args, **kwargs)
 
-@overrideMethod(tooltips, 'getUnlockPrice')
+
+
+#
+# Handlers/Tooltips
+#
+
 @dependency.replace_none_kwargs(itemsCache=IItemsCache)
-def getUnlockPrice(base, compactDescr, parentCD=None, vehicleLevel=UNKNOWN_VEHICLE_LEVEL, blueprintCount=0, itemsCache=None):
+def tooltips_getUnlockPrice(base, compactDescr, parentCD=None, vehicleLevel=UNKNOWN_VEHICLE_LEVEL, blueprintCount=0, itemsCache=None):
     isAvailable, cost, need, defCost, discount = base(compactDescr, parentCD, vehicleLevel, blueprintCount, itemsCache=itemsCache)
     if cfg_hangar_enableFreeXpLocker and not freeXP_enable:
         need += itemsCache.items.stats.actualFreeXP
     return (isAvailable, cost, need, defCost, discount)
 
-# "reimport"
-import gui.shared.tooltips.module as tooltips_module
-import gui.shared.tooltips.vehicle as tooltips_vehicle
-tooltips_module.getUnlockPrice = tooltips.getUnlockPrice
-tooltips_vehicle.getUnlockPrice = tooltips.getUnlockPrice
 
-# force call invalidateFreeXP to update actualFreeXP on vehicle change
-# TODO:1.4.1
-#@overrideMethod(Research, 'onResearchItemsDrawn')
-#def Research_onResearchItemsDrawn(base, self):
-#    base(self)
-#    self.invalidateFreeXP()
+
+#
+# XFW API
+#
+
+__initialized = False
+
+def xfw_module_init():
+    global __initialized
+    if not __initialized:
+        g_eventBus.addListener(XFW_COMMAND.XFW_CMD, onXfwCommand)
+        g_eventBus.addListener(XVM_EVENT.CONFIG_LOADED, onConfigLoaded)
+
+        overrideMethod(shop, 'canBuyGoldForItemThroughWeb')(shop_canBuyGoldForItemThroughWeb)
+        overrideMethod(shop, 'canBuyGoldForVehicleThroughWeb')(shop_canBuyGoldForVehicleThroughWeb)
+
+        overrideMethod(StatsRequester, 'gold')(StatsRequester_gold)
+        overrideMethod(StatsRequester, 'freeXP')(StatsRequester_freeXP)
+        overrideMethod(StatsRequester, 'crystal')(StatsRequester_crystal)
+
+        overrideMethod(HeaderMoneyAndXpTooltipData, '_getValue')(HeaderMoneyAndXpTooltipData_getValue)
+
+        overrideMethod(MoneyBalance, '_MoneyBalance__setStats')(MoneyBalance__setStats)
+
+        overrideMethod(FullScreenDialogView, '_FullScreenDialogView__setStats')(FullScreenDialogView__setStats)
+
+        overrideMethod(TechTree, '_populate')(TechTree_populate)
+        overrideMethod(TechTree, '_dispose')(TechTree_dispose)
+
+        overrideMethod(Research, '_populate')(Research_populate)
+        overrideMethod(Research, '_dispose')(Research_dispose)
+
+        overrideMethod(MainView, '_dispose')(MainView_populate)
+        overrideMethod(MainView, '_dispose')(MainView_dispose) 
+
+        overrideMethod(tooltips, 'getUnlockPrice')(tooltips_getUnlockPrice)
+        import gui.shared.tooltips.module as tooltips_module
+        import gui.shared.tooltips.vehicle as tooltips_vehicle
+        tooltips_module.getUnlockPrice = tooltips.getUnlockPrice
+        tooltips_vehicle.getUnlockPrice = tooltips.getUnlockPrice
+
+        __initialized = True
+    
+
+def xfw_module_fini():
+    global __initialized
+    if __initialized:
+        g_eventBus.removeListener(XFW_COMMAND.XFW_CMD, onXfwCommand)
+        g_eventBus.removeListener(XVM_EVENT.CONFIG_LOADED, onConfigLoaded)
+        __initialized = False
+
+
+def xfw_is_module_loaded():
+    global __initialized
+    return __initialized
