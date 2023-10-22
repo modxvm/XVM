e6c6e672a2f82f1c8bb7f8f9cd8bf83cd6d02780
Paul Ekshmidt <kurzdor@pm.me>
2023-10-22 22:57:31 +0000

    xvm/crew: initial revival of crew auto return only on crewReturnByDefault...

--- src/python/xvm_crew/__init__.py
+++ src/python/xvm_crew/__init__.py
@@ -12,10 +12,13 @@ Copyright (c) 2013-2023 XVM Contributors
 import logging
 
 # BigWorld
-from gui.shared import g_eventBus
-from CurrentVehicle import g_currentVehicle
+import BigWorld
+from PlayerEvents import g_playerEvents
+# from gui.shared import g_eventBus
+from CurrentVehicle import _CurrentVehicle, g_currentVehicle
 from gui.Scaleform.daapi.view.lobby.cyberSport.VehicleSelectorPopup import VehicleSelectorPopup
 from helpers import dependency
+from skeletons.gui.app_loader import IAppLoader, GuiGlobalSpaceID
 from skeletons.gui.shared import IItemsCache
 
 # XFW
@@ -24,14 +27,18 @@ from xfw_actionscript.python import *
 
 # XVM
 import xvm_main.python.config as config
-from xvm_main.python.logger import *
-from xvm_main.python.xvm import l10n
+# from xvm_main.python.xvm import l10n
 import xvm_main.python.userprefs as userprefs
 
 # XVM Crew
 import wg_compat
 
 
+#
+# Logger
+#
+
+logger = logging.getLogger(__name__)
 
 #
 # Constants
@@ -56,34 +63,107 @@ class USERPREFS(object):
 
 
 #
+# Handlers
+#
+
+class Crew(object):
+    itemsCache = dependency.descriptor(IItemsCache)
+
+    def __init__(self):
+        self.intCD = None
+        self.__callbackID = None
+
+    def init(self):
+        vehicle = g_currentVehicle.item
+        intCD = vehicle.intCD
+        self.intCD = intCD
+
+    def invalidate(self):
+        self.intCD = None
+        if self.__callbackID is not None:
+            BigWorld.cancelCallback(self.__callbackID)
+            self.__callbackID = None
+
+    def handleVehicleChange(self):
+        if config.get('hangar/enableCrewAutoReturn') and config.get('hangar/crewReturnByDefault'):
+            if self.__callbackID is not None:
+                BigWorld.cancelCallback(self.__callbackID)
+                self.__callbackID = None
+            vehicle = g_currentVehicle.item
+            intCD = vehicle.intCD
+            if intCD != self.intCD:
+                self.__callbackID = BigWorld.callback(1.5, self.returnCrew)
+                self.intCD = intCD
+
+    def handlePopupSelect(self, items):
+        if len(items) == 1:
+            cd = int(items[0])
+            vehicle = self.itemsCache.items.getItemByCD(cd)
+            if vehicle and vehicle.isInInventory and not (vehicle.isCrewFull or vehicle.isInBattle or vehicle.isLocked):
+                if config.get('hangar/enableCrewAutoReturn'):
+                    if userprefs.get(USERPREFS.AUTO_PREV_CREW + str(vehicle.invID), True):
+                        wg_compat.g_instance.processReturnCrewForVehicleSelectorPopup(vehicle)
+
+    def isCrewAvailable(self):
+        vehicle = g_currentVehicle.item
+        lastCrewIDs = vehicle.lastCrew
+        if lastCrewIDs is None:
+            return False
+        for lastTankmenInvID in lastCrewIDs:
+            actualLastTankman = self.itemsCache.items.getTankman(lastTankmenInvID)
+            if actualLastTankman is not None and actualLastTankman.isInTank:
+                lastTankmanVehicle = self.itemsCache.items.getVehicle(actualLastTankman.vehicleInvID)
+                if lastTankmanVehicle and lastTankmanVehicle.isLocked:
+                    return False
+        return True
+
+    def returnCrew(self):
+        self.__callbackID = None
+        if not g_currentVehicle.isInHangar() or g_currentVehicle.isInBattle() or g_currentVehicle.isLocked() or g_currentVehicle.isCrewFull():
+            return
+        if not self.isCrewAvailable():
+            return
+        wg_compat.g_instance.processReturnCrew()
+
+g_crew = Crew()
+
+#
 # Handlers/XFW
 #
 
 # returns: (result, status)
-def onXfwCommand(cmd, *args):
-    try:
-        if cmd == COMMANDS.PUT_PREVIOUS_CREW:
-            if g_currentVehicle.isInHangar() and not (g_currentVehicle.isCrewFull() or g_currentVehicle.isInBattle() or g_currentVehicle.isLocked()):
-                PutPreviousCrew(g_currentVehicle, False)
-            return (None, True)
-    except Exception:
-        logging.exception('xvm_crew/onXfwCommand')
-        return (None, True)
-    return (None, False)
+# def onXfwCommand(cmd, *args):
+#     try:
+#         if cmd == COMMANDS.PUT_PREVIOUS_CREW:
+#             if g_currentVehicle.isInHangar() and not (g_currentVehicle.isCrewFull() or g_currentVehicle.isInBattle() or g_currentVehicle.isLocked()):
+#                 PutPreviousCrew(g_currentVehicle, False)
+#             return (None, True)
+#     except Exception:
+#         logger.exception('onXfwCommand')
+#         return (None, True)
+#     return (None, False)
 
+#
+# Handlers/AppLoader
+#
 
+def onGUISpaceEntered(spaceID):
+    logger.info('onGUISpaceEntered, spaceID = %s', spaceID)
+    if spaceID == GuiGlobalSpaceID.LOBBY:
+        g_crew.init()
+        g_currentVehicle.onChanged += g_currentVehicle_onChanged
+    elif spaceID in (GuiGlobalSpaceID.LOGIN, GuiGlobalSpaceID.BATTLE, ):
+        g_crew.invalidate()
 
 #
 # Handlers/g_currentVehicle
 #
 
 def g_currentVehicle_onChanged():
-    if config.get('hangar/enableCrewAutoReturn'):
-        vehicle = g_currentVehicle.item
-        invID = g_currentVehicle.invID if vehicle is not None else 0
-        isElite = vehicle.isElite if vehicle is not None else 0
-        as_xfw_cmd(COMMANDS.AS_VEHICLE_CHANGED, invID, isElite)
-
+    try:
+        g_crew.handleVehicleChange()
+    except:
+        logger.exception('g_currentVehicle_onChanged')
 
 #
 # Handlers/VehicleSelectorPopup
@@ -92,18 +172,9 @@ def g_currentVehicle_onChanged():
 def VehicleSelectorPopup_onSelectVehicles(base, self, items):
     base(self, items)
     try:
-        if len(items) == 1:
-            cd = int(items[0])
-            itemsCache = dependency.instance(IItemsCache)
-            vehicle = itemsCache.items.getItemByCD(cd)
-            if vehicle and vehicle.isInInventory and not (vehicle.isCrewFull or vehicle.isInBattle or vehicle.isLocked):
-                if config.get('hangar/enableCrewAutoReturn'):
-                    if userprefs.get(USERPREFS.AUTO_PREV_CREW + str(vehicle.invID), True):
-                        wg_compat.g_instance.processReturnCrewForVehicleSelectorPopup(vehicle)
-    except Exception:
-        logging.exception('xvm_crew/VehicleSelectorPopup_onSelectVehicles')
-
-
+        g_crew.handlePopupSelect(items)
+    except:
+        logger.exception('VehicleSelectorPopup_onSelectVehicles')
 
 #
 # XFW API
@@ -114,17 +185,21 @@ __initialized = False
 def xfw_module_init():
     global __initialized
     if not __initialized:
-        g_eventBus.addListener(XFW_COMMAND.XFW_CMD, onXfwCommand)
-        g_currentVehicle.onChanged += g_currentVehicle_onChanged
+        # g_eventBus.addListener(XFW_COMMAND.XFW_CMD, onXfwCommand)
+        dependency.instance(IAppLoader).onGUISpaceEntered += onGUISpaceEntered
         overrideMethod(VehicleSelectorPopup, 'onSelectVehicles')(VehicleSelectorPopup_onSelectVehicles)
         __initialized = True
-    
+
 
 def xfw_module_fini():
     global __initialized
     if __initialized:
+        try:
+            dependency.instance(IAppLoader).onGUISpaceEntered += onGUISpaceEntered
+        except dependency.DependencyError:
+            pass
         g_currentVehicle.onChanged -= g_currentVehicle_onChanged
-        g_eventBus.removeListener(XFW_COMMAND.XFW_CMD, onXfwCommand)
+        # g_eventBus.removeListener(XFW_COMMAND.XFW_CMD, onXfwCommand)
         __initialized = False
 
 
--- src/python/xvm_crew/wg_compat.py
+++ src/python/xvm_crew/wg_compat.py
@@ -22,9 +22,7 @@ from gui.shared.utils import decorators
 class _WGCompat():
 
     @decorators.adisp_process('crewReturning')
-    def processReturnCrew(self, print_message = True):
-        if not g_currentVehicle.isInHangar() or g_currentVehicle.isInBattle() or g_currentVehicle.isLocked() or g_currentVehicle.isCrewFull():
-            return
+    def processReturnCrew(self, print_message=True):
         result = yield TankmanReturn(g_currentVehicle.item).request()
         if len(result.userMsg) and print_message:
             SystemMessages.pushI18nMessage(result.userMsg, type=result.sysMsgType)
