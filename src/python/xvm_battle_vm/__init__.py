ef727a702b03ee051114733d3f5f15ae98b38a75
Kurzdor <gitlab@kurzdor.me>
2024-04-08 18:57:54 +0300

    python/battle_vm: temporarily disable vehicle markers

--- src/python/xvm_battle_vm/__init__.py
+++ src/python/xvm_battle_vm/__init__.py
@@ -3,6 +3,8 @@ SPDX-License-Identifier: GPL-3.0-or-later
 Copyright (c) 2013-2024 XVM Contributors
 """
 
+from xfw import getRegion
+
 #
 # XFW API
 #
@@ -11,37 +13,40 @@ __initialized = False
 
 def xfw_module_init():
     global __initialized
-    if not __initialized:
-        import vehicleMarkers
-        vehicleMarkers.init()
 
-        import eventsAvatar
-        eventsAvatar.init()
-        
-        import eventsBSDC
-        eventsBSDC.init()
-        
-        import eventsMM
-        eventsMM.init()
+    if not __initialized:
+        # Temporary disable on WG 1.24.1
+        if getRegion() == 'RU':
+            import vehicleMarkers
+            vehicleMarkers.init()
+
+            import eventsAvatar
+            eventsAvatar.init()
+
+            import eventsBSDC
+            eventsBSDC.init()
+
+            import eventsMM
+            eventsMM.init()
 
         __initialized = True
-
 
 
 def xfw_module_fini():
     global __initialized
     if __initialized:
-        import vehicleMarkers
-        vehicleMarkers.fini()
+        if getRegion() == 'RU':
+            import vehicleMarkers
+            vehicleMarkers.fini()
 
-        import eventsAvatar
-        eventsAvatar.fini()
-        
-        import eventsBSDC
-        eventsBSDC.fini()
-        
-        import eventsMM
-        eventsMM.fini()
+            import eventsAvatar
+            eventsAvatar.fini()
+
+            import eventsBSDC
+            eventsBSDC.fini()
+
+            import eventsMM
+            eventsMM.fini()
 
         __initialized = False
 
