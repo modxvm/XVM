51cd0593e0b85131c408a77f079f0a6f4bfdd0ae
Kurzdor <gitlab@kurzdor.me>
2024-10-15 09:30:13 +0300

    python/techtree: refactor module

--- src/python/xvm_techtree/__init__.py
+++ src/python/xvm_techtree/__init__.py
@@ -3,36 +3,44 @@ SPDX-License-Identifier: GPL-3.0-or-later
 Copyright (c) 2013-2024 XVM Contributors
 """
 
-#####################################################################
-# imports
+#
+# Imports
+#
 
+# stdlib
+import logging
 import traceback
 
+# BigWorld
 from gui.Scaleform.daapi.view.lobby.techtree.techtree_dp import _TechTreeDataProvider
 from gui.Scaleform.daapi.view.meta.ModuleInfoMeta import ModuleInfoMeta
 from helpers import dependency
 from skeletons.gui.shared import IItemsCache
 
+# XFW
 from xfw import *
 
+# XVM Main
 import xvm_main.python.config as config
 from xvm_main.python.vehinfo import _getRanges
-from xvm_main.python.logger import *
 from xvm_main.python.xvm import l10n
 
 
-#####################################################################
-# handlers
 
-@overrideMethod(_TechTreeDataProvider, 'getAllVehiclePossibleXP')
+#
+# Handlers
+#
+
 def _TechTreeDataProvider_getAllVehiclePossibleXP(base, self, nodeCD, unlockStats):
-    if not config.get('hangar/allowExchangeXPInTechTree'):
-        return unlockStats.getVehTotalXP(nodeCD)
+    try:
+        if not config.get('hangar/allowExchangeXPInTechTree'):
+            return unlockStats.getVehTotalXP(nodeCD)
+    except Exception:
+        logging.getLogger('XVM/TechTree').exception('_TechTreeDataProvider_getAllVehiclePossibleXP')
     return base(self, nodeCD, unlockStats)
 
 
 # add shooting range in gun info window for SPG/machine guns
-@overrideMethod(ModuleInfoMeta, 'as_setModuleInfoS')
 def ModuleInfoWindow_as_setModuleInfoS(base, self, moduleInfo):
     try:
         if moduleInfo['type'] == 'vehicleGun':
@@ -40,12 +48,41 @@ def ModuleInfoWindow_as_setModuleInfoS(base, self, moduleInfo):
             itemsCache = dependency.instance(IItemsCache)
             vehicle = itemsCache.items.getItemByCD(veh_id)
             gun = itemsCache.items.getItemByCD(self.moduleCompactDescr).descriptor
-            turret = self._ModuleInfoWindow__vehicleDescr.turret    # not accurate, but not relevant here
+            # not accurate, but not relevant here
+            turret = self._ModuleInfoWindow__vehicleDescr.turret
             (viewRange, shellRadius, artiRadius) = _getRanges(turret, gun, vehicle.nationName, vehicle.type)
-            if vehicle.type == 'SPG':   # arti
+            if vehicle.type == 'SPG':
                 moduleInfo['parameters']['params'].append({'type': '<h>' + l10n('shootingRadius') + ' <p>' + l10n('(m)') + '</p></h>', 'value': '<h>' + str(artiRadius) + '</h>'})
-            elif shellRadius < 707:     # not arti, short range weapons
+            # not arty, short range weapons
+            elif shellRadius < 707:
                 moduleInfo['parameters']['params'].append({'type': '<h>' + l10n('shootingRadius') + ' <p>' + l10n('(m)') + '</p></h>', 'value': '<h>' + str(shellRadius) + '</h>'})
-    except Exception as ex:
-        err(traceback.format_exc())
+    except Exception:
+        logging.getLogger('XVM/TechTree').exception('ModuleInfoWindow_as_setModuleInfoS')
     return base(self, moduleInfo)
+
+
+
+#
+# XFW API
+#
+
+__initialized = False
+
+def xfw_module_init():
+    global __initialized
+    if not __initialized:
+        overrideMethod(_TechTreeDataProvider, 'getAllVehiclePossibleXP')(_TechTreeDataProvider_getAllVehiclePossibleXP)
+        overrideMethod(ModuleInfoMeta, 'as_setModuleInfoS')(ModuleInfoWindow_as_setModuleInfoS)
+
+        __initialized = True
+
+
+def xfw_module_fini():
+    global __initialized
+    if __initialized:
+        __initialized = False
+
+
+def xfw_is_module_loaded():
+    global __initialized
+    return __initialized
